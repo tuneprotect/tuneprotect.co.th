cebbdb5f47e96d087a4acb098d3068ef747ad47d
sikaruagency <sikaruagency@gmail.com>
2021-06-19 00:09:27 +0700

    fix

--- resources/views/frontend/page/payment.blade.php
+++ resources/views/frontend/page/payment.blade.php
@@ -3,11 +3,11 @@
 @section('page')
     <form id="frm_test" method="post" action="{{config('payment.link')}}">
         @foreach ($arr_post as $k => $v)
-            <div style="display: block"><label>{{$k}}</label><input type="text" name="{{$k}}" value="{{$v}}"/></div>
+            <div style="display: none"><label>{{$k}}</label><input type="hidden" name="{{$k}}" value="{{$v}}"/></div>
         @endforeach
         <input style="display: block" type="submit" name="btn_submit" value="Confirm"/>
     </form>
-{{--    <script type="text/javascript">--}}
-{{--        document.getElementById("frm_test").submit();--}}
-{{--    </script>--}}
+    <script type="text/javascript">
+        document.getElementById("frm_test").submit();
+    </script>
 @endsection
