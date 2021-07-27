5e5deec5fe98c1b7468a9d4e812262f3ea2b81a3
weerapol <weerapol.w@gmail.com>
2021-07-27 12:20:12 +0700

    Add Link project AMADO

--- config/tune-api.php
+++ config/tune-api.php
@@ -3,4 +3,5 @@ return [
     'url' => env('TUNE_API_URL', 'http://webtest1.tuneinsurance.co.th/tunepolicy/api/WEBSITE/'),
     'user' => env('TUNE_API_USER', 'TPTWEBSITE'),
     'password' => env('TUNE_API_PASSWORD', 'TPTWEBSITE@123'),
+    'amado_link' => env('TUNE_API_AMADO_LINK', 'http://webtest1.tuneinsurance.co.th/WebPortal/Pages/CovidRM.aspx'),
 ];
