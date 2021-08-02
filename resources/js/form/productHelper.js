c7abf30cd00551f9cbe6de63a63e601c26c09378
weerapol <weerapol.w@gmail.com>
2021-08-02 17:30:11 +0700

    ci protal plan not change

--- resources/js/form/productHelper.js
+++ resources/js/form/productHelper.js
@@ -4,7 +4,7 @@ import {showDateError, showFieldError} from "../validate_form";
 import Swal from "sweetalert2";
 
 export const getPackageData = async (currentPackage,channel) => {
-    let res = await fetch(`/storage/json/${currentPackage.toLowerCase() + (channel ? '_'+channel : '') }.json?454654748`);
+    let res = await fetch(`/storage/json/${currentPackage.toLowerCase() + (channel ? '_'+channel : '') }.json?4546547489`);
     return await res.json();
 }
 
