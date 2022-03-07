3b61f2d3376ad16b372c5a273779c7b84bae64fd
weerapol <weerapol.w@gmail.com>
2022-03-07 09:27:04 +0700

    tune ipass

--- resources/js/form/productHelper.js
+++ resources/js/form/productHelper.js
@@ -4,7 +4,7 @@ import {showDateError, showFieldError} from "../validate_form";
 import Swal from "sweetalert2";
 
 export const getPackageData = async (currentPackage,channel) => {
-    let res = await fetch(`/storage/json/${currentPackage.toLowerCase() + (channel ? '_'+channel : '') }.json?1646280879`);
+    let res = await fetch(`/storage/json/${currentPackage.toLowerCase() + (channel ? '_'+channel : '') }.json?1646619345`);
     return await res.json();
 }
 
