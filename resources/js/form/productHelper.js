f7badf2cef4c75a93e24c8027bc4def3ef9dfcd7
weerapol <weerapol.w@gmail.com>
2022-03-14 11:14:25 +0700

    UAT iPass Covid#2

--- resources/js/form/productHelper.js
+++ resources/js/form/productHelper.js
@@ -4,7 +4,7 @@ import {showDateError, showFieldError} from "../validate_form";
 import Swal from "sweetalert2";
 
 export const getPackageData = async (currentPackage,channel) => {
-    let res = await fetch(`/storage/json/${currentPackage.toLowerCase() + (channel ? '_'+channel : '') }.json?1647226452`);
+    let res = await fetch(`/storage/json/${currentPackage.toLowerCase() + (channel ? '_'+channel : '') }.json?1647230395`);
     return await res.json();
 }
 
