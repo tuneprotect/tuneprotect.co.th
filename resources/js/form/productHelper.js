f11f14526d52caf43e65c5a4e605b6c08aae8778
weerapol <weerapol.w@gmail.com>
2022-03-16 23:18:24 +0700

    MA Covid DL

--- resources/js/form/productHelper.js
+++ resources/js/form/productHelper.js
@@ -9,7 +9,7 @@ export const getPackageData = async (currentPackage,channel) => {
 }
 
 export const getNationalityData = async () => {
-    let res = await fetch(`/storage/json/nationality.json?1645082151`);
+    let res = await fetch(`/storage/json/nationality.json?1647230395`);
     return await res.json();
 }
 export const getNationalityDataTH = async () => {
