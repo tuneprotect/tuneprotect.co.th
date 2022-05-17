9202ee5081f4ba511839876a70c6a0e734b1f58b
weerapol <weerapol.w@gmail.com>
2022-05-17 18:00:40 +0700

    iPass Covid 19 18May2022 #2

--- resources/js/form/productHelper.js
+++ resources/js/form/productHelper.js
@@ -4,7 +4,7 @@ import {showDateError, showFieldError} from "../validate_form";
 import Swal from "sweetalert2";
 
 export const getPackageData = async (currentPackage,channel) => {
-    let res = await fetch(`/storage/json/${currentPackage.toLowerCase() + (channel ? '_'+channel : '') }.json?1652164250`);
+    let res = await fetch(`/storage/json/${currentPackage.toLowerCase() + (channel ? '_'+channel : '') }.json?1652785119`);
     return await res.json();
 }
 
