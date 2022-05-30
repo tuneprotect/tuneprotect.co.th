49db5b1a3b64fa965352dd6efced69f93aca0bea
weerapol <weerapol.w@gmail.com>
2022-05-30 11:50:27 +0700

    Consent DSAR#3.

--- resources/views/frontend/page/cookies_consent_mng.blade.php
+++ resources/views/frontend/page/cookies_consent_mng.blade.php
@@ -6,7 +6,7 @@
         <article class="wrapper">
             <h1>การจัดการความยินยอม (Consent Management)</h1><br>
             <h4><a href="https://privacyportaluatde.onetrust.com/hosted-webform/consent/57ee11fd-b018-48a4-be73-4abc0f30bbbe/ecd11121-937b-485f-b4eb-70d1bd28bf10" target="_blank">
-                    1. แบบฟอร์มให้ความยินยอมในการเปิดเผยข้อมูลต่อกรมสรรพากรเพื่อสิทธิประโยชน์ทางภาษี</a></h4><br>
+                    1. สำหรับผู้เอาประกันภัย และบุคคลทั่วไป</a></h4><br>
             <h4><a href="https://privacyportaluatde.onetrust.com/hosted-webform/consent/57ee11fd-b018-48a4-be73-4abc0f30bbbe/534cbf2a-0e4f-4497-8b91-64757a4464b2" target="_blank">
                     2. สำหรับตัวแทนประกันภัย และนายหน้าประกันภัย </a></h4><br>
             <h4><a href="https://privacyportaluatde.onetrust.com/hosted-webform/consent/57ee11fd-b018-48a4-be73-4abc0f30bbbe/74d03a17-1a23-4e20-b711-e08713d06b1f" target="_blank">
