ac4a8df2103b56b74f2482a3ad3bb777f7c20fc1
weerapol <weerapol.w@gmail.com>
2022-05-31 15:11:32 +0700

    Consent DSAR Production#1

--- resources/views/frontend/page/cookies_dsar.blade.php
+++ resources/views/frontend/page/cookies_dsar.blade.php
@@ -18,10 +18,14 @@
                     border: none;
                 }
             </style>
+
+{{--            UAT--}}
             <div class="ot-form-wrapper">
-                <iframe src="https://privacyportaluatde.onetrust.com/webform/57ee11fd-b018-48a4-be73-4abc0f30bbbe/9a9a04ad-b58a-42b7-80c7-448abc656867">
-                </iframe>
+{{--                <iframe src="https://privacyportaluatde.onetrust.com/webform/57ee11fd-b018-48a4-be73-4abc0f30bbbe/9a9a04ad-b58a-42b7-80c7-448abc656867"></iframe>--}}
+                <iframe src="https://privacyportal-apac.onetrust.com/webform/c77257a4-4ff5-4509-be4e-5197730f8725/90a86f78-9652-4a55-8968-514c08d6a775"></iframe>
+
             </div>
+
             <!-- OneTrust Web Form end -->
         </article>
         <br>
