fcb3ba33f94e9e12f614bb1cb3f20dbab0e2cb44
weerapol <weerapol.w@gmail.com>
2022-06-21 19:31:29 +0700

    Merge Baowan#11

--- resources/js/form/productHelper.js
+++ resources/js/form/productHelper.js
@@ -4,7 +4,7 @@ import {showDateError, showFieldError} from "../validate_form";
 import Swal from "sweetalert2";
 
 export const getPackageData = async (currentPackage,channel) => {
-    let res = await fetch(`/storage/json/${currentPackage.toLowerCase() + (channel ? '_'+channel : '') }.json?202206081935`);
+    let res = await fetch(`/storage/json/${currentPackage.toLowerCase() + (channel ? '_'+channel : '') }.json?1655814514`);
     return await res.json();
 }
 
