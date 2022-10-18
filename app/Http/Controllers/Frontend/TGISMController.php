7c379105b0f53cb80ac78919ee3764ffee60c37d
tuneprotect <yutthana.s@tuneprotect.com>
2022-10-18 10:55:51 +0700

    no message

--- app/Http/Controllers/Frontend/TGISMController.php
+++ app/Http/Controllers/Frontend/TGISMController.php
@@ -83,7 +83,7 @@ class TGISMController  extends ProductController
              $this->bodyData['massage_key'] = '';
 
              //return parent::index('coronavirus-covid-19-insurance','ONTGISM',$memberId);
-             return parent::index('coronavirus-covid-19-insurance','TAISMTG',$memberId);
+             return parent::index('coronavirus-covid-19-insurance','ONTGISM',$memberId);
 
          }
 
