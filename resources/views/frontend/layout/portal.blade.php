911b82ffe77845a459848d2ba79ded2a52cd0e40
tuneprotect <yutthana.s@tuneprotect.com>
2022-10-21 11:10:32 +0700

    no message

--- resources/views/frontend/layout/portal.blade.php
+++ resources/views/frontend/layout/portal.blade.php
@@ -146,7 +146,7 @@
                                                      alt="{{$v->locales[$locale]->title}}">
                                             </picture>
                                         @elseif($selected==='TGCVLP')
-                                            <picture>
+                                            <picture class="aaa">
                                                 <source media="(min-width:768px)" srcset="/storage/Banner/Banner_TG_D.jpg">
                                                 <img src="{{url(!empty($v->pic_mobile_en) ? "/storage/Banner/Banner_TG_D.jpg" : "/storage/Banner/Banner_TG_M.jpg" )}}"
                                                      alt="{{$v->locales[$locale]->title}}">
