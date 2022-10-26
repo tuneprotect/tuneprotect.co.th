e65f889d1dd652a99278774433ca5850ed3bc41b
tuneprotect <yutthana.s@tuneprotect.com>
2022-10-26 11:51:47 +0700

    no message

--- resources/views/frontend/page/index.blade.php
+++ resources/views/frontend/page/index.blade.php
@@ -90,7 +90,7 @@
                                     </div>
                                     <ul>
                                         @foreach($v->productPackage AS $v1)
-                                            @if($v1->code === 'ONVACINA' || $v1->code === 'CVCARE'|| $v1->code === 'ONCOVIDMW' || $v1->code === 'TGCVLP' ||  $v1->code === 'ONVSUREA')
+                                            @if($v1->code === 'ONVACINA' || $v1->code === 'CVCARE'|| $v1->code === 'ONCOVIDMW' || $v1->code === 'TGCVLP' || $v1->code === 'TAISMTG' || $v1->code === 'ONVSUREA')
                                             @else
                                                 <li>
                                                     <a
