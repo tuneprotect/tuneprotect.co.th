c36fb89524e38e927619d2c191b74d3c70f952e4
tuneprotect <yutthana.s@tuneprotect.com>
2022-10-26 16:42:59 +0700

    edit fdToDate

--- resources/views/frontend/form/taipoct22.blade.php
+++ resources/views/frontend/form/taipoct22.blade.php
@@ -25,10 +25,10 @@
                 <div class="controls-wrapper">
                     <input id="fdFromDate" name="fdFromDate" type="text" class="flatpickr"
                            data-mindate="{{date('Y-m-d')}}"
-                           data-maxdate="{{date('Y-m-d',strtotime( "+8 months"))}}"
+                           data-maxdate="{{date('Y-m-d',strtotime( "+180 days"))}}"
                            data-setmindatetoequal ="fdToDate"
                            data-setmaxdateto="fdToDate"
-                           data-setmaxdaterange="364"
+                           data-setmaxdaterange="180"
                            data-error="@lang('product.error.travel_start_date')"
                     />
                     <label for="fdFromDate">@lang("product.ipass_arrival_date")</label>
