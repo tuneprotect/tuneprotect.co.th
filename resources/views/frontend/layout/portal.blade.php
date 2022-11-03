e29bd34336411528612176d3358ddff7051ec330
tuneprotect <yutthana.s@tuneprotect.com>
2022-11-03 22:14:11 +0700

    no message

--- resources/views/frontend/layout/portal.blade.php
+++ resources/views/frontend/layout/portal.blade.php
@@ -100,10 +100,10 @@
                             @if(!empty($v->pic_en))
                                 @if($selected==='ONVACINA' || $selected === 'ONVSUREA')
                                         <picture>
-                                            <source media="(min-width:768px)" srcset="{{url(str_replace('Banner_Covid_D_EN', 'Banner_VSURE_D_EN', $v->pic_en))}}">
+                                        <source media="(min-width:768px)" srcset="{{url(str_replace('Banner_Covid_D_EN', 'Banner_VSURE_D_EN', $v->pic_en))}}">
                                                 <img src="{{url(!empty($v->pic_mobile_en) ? url(str_replace('Banner_Covid_D_EN', 'Banner_VSURE_D_EN', $v->pic_mobile_en)) : url(str_replace('Banner_Covid_D_EN', 'Banner_VSURE_M_EN', $v->pic_en)) )}}"
-                                                alt="{{$v->locales[$locale]->title}}">
-                                            </picture>
+                                                    alt="{{$v->locales[$locale]->title}}">
+                                                    </picture>
                                 @elseif($selected==='ONCOVIDMW')
                                     <picture>
                                         <source media="(min-width:768px)" srcset="{{url(str_replace('Banner_iPass_COVID', 'Migrant-Banner-En01', $v->pic_en))}}">
@@ -118,27 +118,20 @@
                                             </picture>
                                 @elseif(isset($partner))
                                         @if($partner==='LUMA' && ($selected==='ONTALN' || $selected==='TAIPOCT22'))
-                                            <picture  class="cccc">
+                                            <picture>
                                                 <source media="(min-width:768px)" srcset="/storage/Banner/Banner_Luma_D.jpg">
                                                 <img src="{{url(!empty($v->pic_mobile_en) ? "/storage/Banner/Banner_Luma_D.jpg" : "/storage/Banner/Banner_Luma_M.jpg" )}}"
                                                      alt="{{$v->locales[$locale]->title}}">
                                             </picture>
                                         @elseif($selected==='ONTALN')
-                                            <picture class="bbb">
+                                            <picture>
                                                 <source media="(min-width:768px)"
                                                         srcset="{{url(str_replace('Banner_TA_D_EN', 'Banner_Tune_iPass', $v->pic_en))}}">
                                                 <img
                                                     src="{{url(!empty($v->pic_mobile_en) ? url(str_replace('Banner_TA_D_EN', 'Banner_Tune_iPass', $v->pic_mobile_en)) : url(str_replace('Banner_TA_D_EN', 'Banner_Tune_iPass', $v->pic_en)) )}}"
                                                     alt="{{$v->locales[$locale]->title}}">
                                             </picture>
-                                        @elseif($selected==='TAIPOCT22')
-                                            <picture class="aaa">
-                                                <source media="(min-width:768px)"
-                                                        srcset="{{url(str_replace('Banner_TA_D_EN', 'Banner_Tune_iPass', $v->pic_en))}}">
-                                                <img
-                                                    src="{{url(!empty($v->pic_mobile_en) ? url(str_replace('Banner_TA_D_EN', 'Banner_Tune_iPass', $v->pic_mobile_en)) : url(str_replace('Banner_TA_D_EN', 'Banner_Tune_iPass', $v->pic_en)) )}}"
-                                                    alt="{{$v->locales[$locale]->title}}">
-                                            </picture>
+                                       
                                     @elseif($selected==='TAISM')
                                         <picture>
                                             <source media="(min-width:768px)"
@@ -177,7 +170,7 @@
                                                      alt="{{$v->locales[$locale]->title}}">
                                             </picture>
                                         @else
-                                            <picture  class="xxxxx">
+                                            <picture>
                                                 <source media="(min-width:768px)" srcset="{{url($v->pic_en)}}">
                                                 <img src="{{url(!empty($v->pic_mobile_en) ? $v->pic_mobile_en : $v->pic_en )}}"
                                                      alt="{{$v->locales[$locale]->title}}">
@@ -185,7 +178,7 @@
                                         @endif
 
                                     @else
-                                            <picture  class="xxxxx">
+                                            <picture>
                                                 <source media="(min-width:768px)" srcset="{{url($v->pic_en)}}">
                                                 <img src="{{url(!empty($v->pic_mobile_en) ? $v->pic_mobile_en : $v->pic_en )}}"
                                                     alt="{{$v->locales[$locale]->title}}">
@@ -224,16 +217,8 @@
                                         <img src="{{url(!empty($v->pic_mobile_en) ? "/storage/Banner/Banner_TAI_D.jpg" : "/storage/Banner/Banner_TAI_M.jpg" )}}"
                                                 alt="{{$v->locales[$locale]->title}}">
                                     </picture>
-                                @elseif($selected==='TAIPOCT22')
-                                    <picture class="aaaxx">
-                                        <source media="(min-width:768px)"
-                                                srcset="{{url(str_replace('Banner_TA_D_EN', 'Banner_Tune_iPass', $v->pic_en))}}">
-                                        <img
-                                            src="{{url(!empty($v->pic_mobile_en) ? url(str_replace('Banner_TA_D_EN', 'Banner_Tune_iPass', $v->pic_mobile_en)) : url(str_replace('Banner_TA_D_EN', 'Banner_Tune_iPass', $v->pic_en)) )}}"
-                                            alt="{{$v->locales[$locale]->title}}">
-                                    </picture>
                                 @else
-                                    <picture  class="xxxxx">
+                                    <picture>
                                         <source media="(min-width:768px)" srcset="{{url($v->pic)}}">
                                         <img src="{{url(!empty($v->pic_mobile) ? $v->pic_mobile : $v->pic )}}"
                                              alt="{{$v->locales[$locale]->title}}">
