a0fa5fa8194e42b483e0711e30c2c9911b708eb8
tuneprotect <yutthana.s@tuneprotect.com>
2022-11-04 16:41:03 +0700

    no message

--- resources/views/frontend/page/portal.blade.php
+++ resources/views/frontend/page/portal.blade.php
@@ -8,7 +8,7 @@
                     <h1 class="product-header">iSafe</h1>
                 @else
                     @if(isset($partner))
-                        @if($partner==='LUMA' && ($selected==='ONTALN' || $selected==='TAIPOCT22'))
+                        @if(($partner==='LUMA' || $partner==='Luma') && ($selected==='ONTALN' || $selected==='TAIPOCT22'))
                             <h1 class="product-header">Luma Thailand Pass</h1>
                         @else
                             <h1 class="product-header">{{$current_package->locales[$locale]->title}}</h1>
