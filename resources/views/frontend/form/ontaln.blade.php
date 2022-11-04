9e1f2f2b4cd307f5e39b1260d634d29ac4453c21
tuneprotect <yutthana.s@tuneprotect.com>
2022-11-04 16:49:39 +0700

    no message

--- resources/views/frontend/form/ontaln.blade.php
+++ resources/views/frontend/form/ontaln.blade.php
@@ -1,5 +1,9 @@
 @include('frontend.component.form-stepper')
-
+<style>
+    header{
+        display:none;
+    }
+</style>
 @if(isset($portal_key))
     @if($portal_key == '2TRZB6Y3NDEQB9E32JUXXGSF2Q5ZB6EXPAKNNGDRD8UXN4Q228UQN4RR22')
         <!-- Activity name for this tag: TuneProtect-LandingPageViews -->
