17b94b2ed68e61ed97c1f27e29b6b8632a53e881
tuneprotect <yutthana.s@tuneprotect.com>
2022-11-09 15:48:02 +0700

    no message

--- app/Http/Controllers/Frontend/ProductController.php
+++ app/Http/Controllers/Frontend/ProductController.php
@@ -269,11 +269,11 @@ class ProductController extends BaseController
                         {
                             //dd(session('partner'));
                     
-                            if(session('partner')==='LUMA' || session('partner')==='Luma'){
+                            // if(session('partner')==='LUMA' || session('partner')==='Luma'){
                                 
-                            }else{
-                                return $this->genView('frontend.page.error');
-                            }
+                            // }else{
+                            //     return $this->genView('frontend.page.error');
+                            // }
                             
                             
                            
@@ -1075,7 +1075,6 @@ class ProductController extends BaseController
             $data = $v->data;
             if($v->result)
             {
-                dd($data);
                 $request->session()->put('doc_no',  $v->result['message']);
                 $request->session()->put('return_link', $request->input('user_defined_2'));
                 $request->session()->put('partner', $request->input('user_defined_3'));
