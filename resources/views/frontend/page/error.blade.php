99fab8c58be65c8561ef7534de91b4795fb5ca5a
tuneprotect <yutthana.s@tuneprotect.com>
2022-11-09 10:33:26 +0700

    no message

--- resources/views/frontend/page/error.blade.php
+++ resources/views/frontend/page/error.blade.php
@@ -6,6 +6,7 @@
             <div class="inner-wrapper">
                 <div>
                     @lang("product.errorpage.content")
+                   {!! @str_replace('{point}',$point,@str_replace('{link}',$return_link,  @str_replace('{doc_no}'," ".$doc_no,$content->locales[$locale]->content)))   !!}</div>
                 </div>
             </div>
         </article>
