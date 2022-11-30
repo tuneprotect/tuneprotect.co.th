92d8987bc67bac785c39b8dfaa693f500c798dd0
tuneprotect <yutthana.s@tuneprotect.com>
2022-11-30 13:37:06 +0700

    no message

--- app/Http/Controllers/Frontend/ProductController.php
+++ app/Http/Controllers/Frontend/ProductController.php
@@ -219,8 +219,6 @@ class ProductController extends BaseController
 
         if (Storage::disk('public')->exists('json/' . $packageJson . '.json')) {
             $package_detail = json_decode(Storage::disk('public')->get('json/' . $packageJson . '.json'));
-            
-            echo var_dump($package_detail);exit();
             foreach ($package_detail as $k => $v) {
                 if (str_starts_with($k, $selected)) {
                     if($this->locale === 'en')
