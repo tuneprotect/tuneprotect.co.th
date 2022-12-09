7105343d787c128bf124a2e0904d24017f9b3cb5
tuneprotect <kriankrai.t@tuneprotect.co.th>
2022-12-09 15:17:08 +0700

    Change popup dialog

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -46,7 +46,7 @@ class IndexController extends BaseController
 
 
         $this->bodyData['overlayComponent'] = 'frontend.component.overlay-form';
-        $this->bodyData['overlayCloseComponent'] = 'frontend.component.overlay-close-form-happytimes';
+        // $this->bodyData['overlayCloseComponent'] = 'frontend.component.overlay-close-form-happytimes';
 
 
         $this->bodyData['service'] = WebContent::where('type_id', ProjectEnum::WEB_CONTENT_SERVICE)
---
+++ resources/views/frontend/component/overlay-in-form-tune-award.blade.php
@@ -0,0 +1,77 @@
+<style>
+
+
+    /*@media all and (min-width: 1080px) {
+    /*    .popup-wrapper {
+    /*        min-width: 1080px;
+    /*    }
+    /*}
+
+    /*@media all and (min-width: 768px) {*/
+    /*    .overlay-section {*/
+    /*        display: flex;*/
+    /*        margin-left: auto;*/
+    /*        margin-right: auto;*/
+    /*    }*/
+    /*}*/
+
+    .overlay-section.move-up {
+        transform: translateY(-50px);
+    }
+
+    /*.overlay-section address {*/
+    /*    padding: 30px;*/
+    /*    flex: 1 1 350px;*/
+    /*}*/
+
+    .overlay-section form {
+        padding: 15px;
+        flex: 1 1 650px;
+        border-radius: 20px;
+        /*background: #e3306c;*/
+        /*background: linear-gradient(152deg, #e3306c 0%, #f7e01f 100%);*/
+    }
+
+    .overlay-section form h3,
+    .overlay-section form cite {
+        color: #ffffff;
+    }
+
+    .overlay-section form .btn-wrapper {
+        text-align: center;
+    }
+
+    #overlayform-section {
+        z-index: 0;
+    }
+
+    /*@media all and (max-width: 600px) {*/
+    /*    #overlayform-section .insurance-form .form-inner {*/
+    /*        padding: 0;*/
+    /*    }*/
+    /*}*/
+	@media (min-width: 1200px) {
+            .popup-wrapper{
+                width:35vw !important;
+            }
+        }
+        .popup-wrapper {
+                width:auto;
+        }
+
+</style>
+<div class="popup-wrapper">
+    <a data-gtm="index-question-close" class="close"><i class="icofont-close"></i></a>
+    <div class="popup-inner-wrapper">
+
+        <div class="overlay-section" id="overlayform-section">
+            <form class="insurance-form" style="text-align: center;" id="frm_overlay">
+                <div class="form-inner">
+                    <picture>                        
+                        <img src="https://www.tuneprotect.co.th/storage/Banner/AW_Dec_2022.jpg" alt="overlay">
+                    </picture>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
