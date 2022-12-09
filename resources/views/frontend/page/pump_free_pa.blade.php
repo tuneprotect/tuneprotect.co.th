d0b9051ba4013b6a6fd23dbd665da0362f28438a
tuneprotect <yutthana.s@tuneprotect.com>
2022-12-09 11:53:07 +0700

    no message

--- resources/views/frontend/page/pump_free_pa.blade.php
+++ resources/views/frontend/page/pump_free_pa.blade.php
@@ -4,54 +4,57 @@
     <main>
         <style>
             #pumpfreepa .section{
-            display: grid;
-            grid-template-columns: repeat(auto-fit,minmax(200px,1fr));
-            -moz-column-gap: 20px;
-            column-gap: 20px;
-            max-width: 700px;
-}
+                display: grid;
+                grid-template-columns: repeat(auto-fit,minmax(200px,1fr));
+                -moz-column-gap: 20px;
+                column-gap: 20px;
+                max-width: 700px;
+            }
+            #pumpfreepa .no-package{
+                display:none !important;
+            }
         </style>
         <div class="-wrapper">
             <section class="wrapper" id="pumpfreepa">
                 <h1>Hello</h1>
                 <div class="sub">
-                            @foreach ($product_category as $k => $v)
-                                <h3 data-index="{{$k}}" {{$k > 0 ?  "class=no-package" : ""}}>{{$v->locales[$locale]->title}} {!! $k > 0 ?  "<i class=icofont-caret-right></i>" : "" !!}</h3>
-                                <div class="section {{$k > 0 ?  "no-package" : ""}}" data-index="{{$k}}">
-                                    @foreach ($product as $v1)
-                                        @if($v->id == $v1->cat_id)
-                                            <div>
-                                                <a class="category" data-gtm="main-nav-product-{{$v1->friendly_url}}"
-                                                   href="{{route('current',['locale' => $locale,'controller' => 'product','func' => $v1->friendly_url],false)}}">
-                                                    @if($v1->productPackage->first())
-                                                        @if($locale == 'en')
-                                                            <img src="{{url($v1->pic_en)}}" alt="">
-                                                        @else
-                                                            <img src="{{url($v1->pic)}}" alt="">
-                                                        @endif
-                                                    @endif
-                                                    <strong>{{$v1->locales[$locale]->title}}</strong>
-                                                </a>
-                                                @if($v1->productPackage)
-                                                    <ul>
-                                                        @foreach ($v1->productPackage as $v2)
-                                                            <li>
-                                                            @if($v2->code === 'ONVACINA' || $v2->code === 'CVCARE'|| $v2->code === 'ONCOVIDMW' || $v2->code === 'TGCVLP' || $v2->code === 'TAISMTG' || $v2->code === 'ONTALN' || $v2->code === 'ONVSUREA')
-                                                            @else
-                                                                    <a data-gtm="main-nav-product-{{$v1->friendly_url}}-{{$v2->code}}"
-                                                                       href="{{route('current',['locale' => $locale,'controller' => 'product','func' => $v1->friendly_url,'params' => $v2->code ])}}">
-                                                                        <span>{{$v2->locales[$locale]->title}}</span></a>
-                                                                @endif
-                                                            </li>
-                                                        @endforeach
-                                                    </ul>
+                    @foreach ($product_category as $k => $v)
+                        <h3 data-index="{{$k}}" {{$k > 0 ?  "class=no-package" : ""}}>{{$v->locales[$locale]->title}} {!! $k > 0 ?  "<i class=icofont-caret-right></i>" : "" !!}</h3>
+                        <div class="section {{$k > 0 ?  "no-package" : ""}}" data-index="{{$k}}">
+                            @foreach ($product as $v1)
+                                @if($v->id == $v1->cat_id)
+                                    <div>
+                                        <a class="category" data-gtm="main-nav-product-{{$v1->friendly_url}}"
+                                            href="{{route('current',['locale' => $locale,'controller' => 'product','func' => $v1->friendly_url],false)}}">
+                                            @if($v1->productPackage->first())
+                                                @if($locale == 'en')
+                                                    <img src="{{url($v1->pic_en)}}" alt="">
+                                                @else
+                                                    <img src="{{url($v1->pic)}}" alt="">
                                                 @endif
-                                            </div>
+                                            @endif
+                                            <strong>{{$v1->locales[$locale]->title}}</strong>
+                                        </a>
+                                        @if($v1->productPackage)
+                                            <ul>
+                                                @foreach ($v1->productPackage as $v2)
+                                                    <li>
+                                                    @if($v2->code === 'ONVACINA' || $v2->code === 'CVCARE'|| $v2->code === 'ONCOVIDMW' || $v2->code === 'TGCVLP' || $v2->code === 'TAISMTG' || $v2->code === 'ONTALN' || $v2->code === 'ONVSUREA')
+                                                    @else
+                                                            <a data-gtm="main-nav-product-{{$v1->friendly_url}}-{{$v2->code}}"
+                                                                href="{{route('current',['locale' => $locale,'controller' => 'product','func' => $v1->friendly_url,'params' => $v2->code ])}}">
+                                                                <span>{{$v2->locales[$locale]->title}}</span></a>
+                                                        @endif
+                                                    </li>
+                                                @endforeach
+                                            </ul>
                                         @endif
-                                    @endforeach
-                                </div>
+                                    </div>
+                                @endif
                             @endforeach
                         </div>
+                    @endforeach
+                </div>
             </section>
         </div>
     </main>
