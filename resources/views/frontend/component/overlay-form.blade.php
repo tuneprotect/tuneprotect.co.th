2b5a4ab946011aa422e1f4360aa88ae847cca612
tuneprotect <kriankrai.t@tuneprotect.co.th>
2022-12-09 15:20:24 +0700

    Remove .blade in file dialog

--- resources/views/frontend/component/overlay-form.blade.php
+++ resources/views/frontend/component/overlay-form.blade.php
@@ -1,5 +1,5 @@
 <div class="page-overlay" style="display: flex;">
-    @include('frontend.component.overlay-in-form-tune-award.blade')
+    @include('frontend.component.overlay-in-form-tune-award')
     <!-- @include('frontend.component.overlay-in-form-itravel') -->
 	<!--@include('frontend.component.overlay-in-form')-->
 </div>
