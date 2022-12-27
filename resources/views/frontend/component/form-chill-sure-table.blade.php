ba13f63960d2b012eee6335d0a4252311fc7473e
tuneprotect <yutthana.s@tuneprotect.com>
2022-12-27 17:05:33 +0700

    no message

--- resources/views/frontend/component/form-chill-sure-table.blade.php
+++ resources/views/frontend/component/form-chill-sure-table.blade.php
@@ -56,7 +56,7 @@
                     </th>
                     <?php $i = 1 ?>
                     @foreach ($package_detail as $k => $v)
-                        <th data-package="{{$k}}">
+                        <th data-package="{{$k}}" class="<?php $i==4 ? 'recommendPackage basePrice':'' ?>" >
                             @if(isset($v->no))
 
                                 <span data-recommend>
