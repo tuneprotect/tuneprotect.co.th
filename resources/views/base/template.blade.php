57a18b8a657c6ebcf7d4db6055821ddfed6597f9
tuneprotect <yutthana.s@tuneprotect.com>
2022-12-28 15:06:35 +0700

    no message

--- resources/views/base/template.blade.php
+++ resources/views/base/template.blade.php
@@ -72,6 +72,18 @@
         {!! $v !!}
     @endforeach
 @endif
+
+
+<script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/plugins/confirmDate/confirmDate.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/plugins/weekSelect/weekSelect.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/plugins/rangePlugin.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/plugins/minMaxTimePlugin.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/plugins/monthSelect/index.js"></script>
+<script src="https://flatpickr.js.org/flatpickr.js"></script>
+
+
+<script src="https://flatpickr.js.org/themer.js"></script>
 </body>
 </html>
 
