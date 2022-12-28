00345e167af2c91d69931fdeec3b1eb73699fc8c
tuneprotect <yutthana.s@tuneprotect.com>
2022-12-28 16:19:20 +0700

    no message

--- resources/js/product/oncshc.js
+++ resources/js/product/oncshc.js
@@ -29,15 +29,4 @@ import Swal from "sweetalert2";
 import {format, parseISO} from "date-fns";
 import intlTelInput from "intl-tel-input";
 
-require('../main');
-require('../product');
 
-validate.validators.idcard = function (value, options, key, attributes) {
-    for (var i = 0, sum = 0; i < 12; i++) {
-        sum += parseFloat(value.charAt(i)) * (13 - i);
-    }
-    const result = ((11 - sum % 11) % 10 === parseFloat(value.charAt(12)));
-    if (!result) {
-        return "^" + $('#fdNationalID').getAttribute('data-error-idcard')
-    }
-};
