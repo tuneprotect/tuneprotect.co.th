757f16623a371e47c2c41dd510c19a488d3025e7
tuneprotect <yutthana.s@tuneprotect.com>
2022-12-28 16:37:02 +0700

    no message

--- resources/views/base/template.blade.php
+++ resources/views/base/template.blade.php
@@ -72,25 +72,6 @@
         {!! $v !!}
     @endforeach
 @endif
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.css">
-	<script src="https://flatpickr.js.org/javascripts/modernizr.js"></script>
-
-
-		<input class="flatpickr flatpickr-input" type="text" placeholder="Select Date.."  data-id="xxx" readonly="readonly">
-		<input class="flatpickr flatpickr-input" type="text" placeholder="Select Date.." data-id="disableSpecific" readonly="readonly">
-	
-	
-
-<script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/plugins/confirmDate/confirmDate.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/plugins/weekSelect/weekSelect.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/plugins/rangePlugin.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/plugins/minMaxTimePlugin.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/plugins/monthSelect/index.js"></script>
-<script src="https://flatpickr.js.org/flatpickr.js"></script>
-
-
-<script src="https://flatpickr.js.org/themer.js"></script>
 </body>
 </html>
 
