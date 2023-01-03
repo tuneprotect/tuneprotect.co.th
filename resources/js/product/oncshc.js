243e8211dec0b7c148da61fccff6bd7e10bcb8c1
tuneprotect <yutthana.s@tuneprotect.com>
2023-01-03 16:14:35 +0700

    no message

--- resources/js/product/oncshc.js
+++ resources/js/product/oncshc.js
@@ -398,7 +398,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     allField.forEach(field => {
         field.addEventListener("change", function (e) {
             validateField(this, constraints);
-            console.log("change",constraints);
             if (['fdName', 'fdSurname', 'fdNationalID'].includes(field.id)) {                
                 validatePolicyStep5(e.target, data.fdPackage);
             }
