5ef304d0017807b886be1d404bc6db816410e113
tuneprotect <yutthana.s@tuneprotect.com>
2023-01-03 09:12:27 +0700

    no message

--- resources/views/base/template.blade.php
+++ resources/views/base/template.blade.php
@@ -78,6 +78,7 @@
 <script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/plugins/rangePlugin.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/plugins/minMaxTimePlugin.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/plugins/monthSelect/index.js"></script>
+<script src="https://flatpickr.js.org/flatpickr.js"></script>
 
 
 <script src="https://flatpickr.js.org/themer.js"></script>
