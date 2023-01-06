65313afec46cbd48e9dd7de9a96122bc15a91cba
tuneprotect <yutthana.s@tuneprotect.com>
2023-01-06 15:10:14 +0700

    no message

--- resources/views/frontend/form/ontaobb2b.blade.php
+++ resources/views/frontend/form/ontaobb2b.blade.php
@@ -6,34 +6,12 @@
             <h3>@lang('product.please_specify_travel_info')</h3>
             <div class="two-col">
                 <div class="controls-wrapper">
-                    <input id="fdFromDate" name="fdFromDate" type="text" class="flatpickr"
-                           data-mindate="{{date('Y-m-d')}}"
-{{--                           data-maxdate="{{date('Y-m-d',strtotime( "+8 months"))}}"--}}
-                           data-setmindatetoequal ="fdToDate"
-                           data-setmaxdateto="fdToDate"
-                           data-setmaxdaterange="29"
-                           data-error="@lang('product.error.travel_start_date')"
-                    />
-                    <label for="fdFromDate">@lang("product.ipass_arrival_date")</label>
-                </div>
-                <div class="controls-wrapper">
-                    <input id="fdToDate" name="fdToDate" type="text" class="flatpickr"
-                           data-mindate="{{date('Y-m-d')}}"
-                           data-error="@lang('product.error.travel_end_date')"/>
-                    <label for="fdToDate">@lang("product.ipass_departure_date")</label>
-                </div>
-
-                <div class="controls-wrapper">
-                    <select id="fdDestFrom" name="fdDestFrom"
-                            data-please-select="@lang('product.please_select')"
-                            data-error="@lang('product.error.country')"
-                    ></select>
-                    <label for="fdDestFrom">@lang("product.departure_country")</label>
-                </div>
-
-                <div class="controls-wrapper">
-                    <select id="fdDestTo" name="fdDestTo" disabled></select>
-                    <label for="fdDestTo">@lang("product.destination_country")</label>
+                    <select id="ctrl_travel_type" name="ctrl_travel_type">
+                            @foreach(__('product.trip_type') as $k => $v)
+                            <option value="{{$k}}">{{$v}}</option>
+                        @endforeach
+                    </select>
+                    <label for="ctrl_travel_type">@lang("product.travel_type")</label>
                 </div>
                 <div class="controls-wrapper">
                     <select id="ctrl_no_of_insured" name="ctrl_no_of_insured">
@@ -44,13 +22,52 @@
                     <label for="ctrl_no_of_insured">@lang("product.no_of_insured")</label>
                 </div>
             </div>
+            <div class="col">
+                <div class="controls-wrapper">
+                    <select id="ctrl_sub_package" name="ctrl_sub_package">
+                        @foreach(__('product.iTravel_zone') as $k => $v)
+                            <option value="{{$k}}">{{$v}}</option>
+                        @endforeach
+                    </select>
+                    <label for="ctrl_sub_package">@lang("product.coverage_period")</label>
+                </div>
+            </div>
+            <div class="col">
+                <div class="controls-wrapper">
+                    <select id="fdDestTo" name="fdDestTo"
+                            data-please-select="@lang('product.please_select')"
+                            data-error="@lang('product.error.country')"></select>
+                    <label for="fdDestTo">@lang("product.destination_country")</label>
+                </div>
+            </div>
+            <div class="two-col">
+                <div class="controls-wrapper">
+                    <input id="fdFromDate" name="fdFromDate" type="text" class="flatpickr"
+                           data-mindate="{{date('Y-m-d',strtotime( "+1 days"))}}"
+                           data-maxdate="{{date('Y-m-d',strtotime( "+8 months"))}}"
+                           data-setmindatetoequal ="fdToDate"
+                           data-setmaxdateto="fdToDate"
+                           data-setmaxdaterange="179"
+                           data-error="@lang('product.error.travel_start_date')"
+                    />
+                    <label for="fdFromDate">@lang("product.departure_date")</label>
+                </div>
+                <div class="controls-wrapper">
+                    <input id="fdToDate" name="fdToDate" type="text" class="flatpickr"
+                           data-mindate="{{date('Y-m-d',strtotime( "+1 days"))}}"
+                           data-error="@lang('product.error.travel_end_date')"/>
+                    <label for="fdToDate">@lang("product.return_date")</label>
+                </div>
+            </div>
+
             <div>
-                <p class="small">@lang('product.ismile_warning')</p>
+                <p class="small">@lang('product.itravel_warning')</p>
             </div>
         </div>
+
+
         <div class="btn-wrapper">
-            <button id="nextstep1" data-gtm="product-{{strtolower($selected)}}-proceed-step-1" data-step="2"
-                    class="btn btn-primary btn-goto">@lang('product.next')</button>
+            <button data-gtm="product-{{strtolower($selected)}}-proceed-step-1" data-step="2" class="btn btn-primary btn-goto">@lang('product.next')</button>
         </div>
 
 
@@ -60,36 +77,33 @@
                 <input type="hidden" id="portal_key" value="{{$portal_key}}"/>
                 <input type="hidden" id="status_api" value="{{$status_api}}"/>
                 <input type="hidden" id="nopayment_status" value="{{$nopayment_status}}"/>
-                <input type="hidden" id="partner" value="{{$partner}}"/>
             </div>
         @endif
 
         <input type="hidden" id="sub_code"/>
+        <input type="hidden" id="all_pack"/>
         <input type="hidden" id="days"/>
 
     </form>
 
     <br/>
     {!! $package->locales[$locale]->remark !!}
-
-
 </section>
-@include('frontend.component.form-coverage-table-with-name',['package_detail' => $package_detail,'selected' =>$selected ])
+
+
+@include('frontend.component.form-itravel-coverage-table-with-subpackage',['package_detail' => $package_detail,'selected' =>$selected ])
 <section style="display: none" id="step3" class="wrapper">
     <form class="insurance-form">
-        <div class="form-head"><span id="form-head"></span></div>
-
+        <div class="form-head"> {{$package->locales[$locale]->title}} <span id="form-head"></span></div>
         <div id="multipleform">
             @for($i = 1;$i < 10;$i++)
                 <div class="form-inner" id="form_profile_{{$i}}" {{$i > 1 ? 'style=display:none;' : ''}}>
 
                     @include('frontend.component.form-profile',[
                              'id_card_field' => 'passport' ,
-                             'has_nationality' => true,
                              'id_card_field_title' => __("product.passport_no") ,
                              'has_birthdate' => true  ,
                              'index' => $i,
-                             'eng_only' => true,
                              'prefix' => 'data_'. $i .'_'])
                     @include('frontend.component.form-address',['prefix' => 'data_'. $i .'_'])
                     @include('frontend.component.form-beneficiary',['prefix' => 'data_'. $i .'_'])
@@ -100,9 +114,10 @@
             @include('frontend.component.form-privacy')
         </div>
         <div class="btn-wrapper">
-            <button data-step="4" class="btn btn-primary btn-goto">@lang('product.proceed')</button>
+            <button data-gtm="product-{{strtolower($selected)}}-proceed-step-3" data-step="4"
+                    class="btn btn-primary btn-goto">@lang('product.proceed')</button>
         </div>
     </form>
 </section>
+@include('frontend.component.form-summary-itravel')
 
-@include('frontend.component.form-summary')
