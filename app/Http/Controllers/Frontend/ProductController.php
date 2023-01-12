bd01df2851c5daf071b5a0098aceb67f03a79e31
tuneprotect <yutthana.s@tuneprotect.com>
2023-01-12 11:03:33 +0700

    no message

--- app/Http/Controllers/Frontend/ProductController.php
+++ app/Http/Controllers/Frontend/ProductController.php
@@ -217,7 +217,7 @@ class ProductController extends BaseController
             }
         }
 
-        
+        dd($packageJson);
         if (Storage::disk('public')->exists('json/' . $packageJson . '.json')) {
             $package_detail = json_decode(Storage::disk('public')->get('json/' . $packageJson . '.json'));            
             foreach ($package_detail as $k => $v) {
