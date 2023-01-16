61348b6d01e19a13edc5ed4dc68a682c130c540a
tuneprotect <yutthana.s@tuneprotect.com>
2023-01-16 13:39:40 +0700

    no message

--- resources/js/product/onb2btad.js
+++ resources/js/product/onb2btad.js
@@ -288,6 +288,9 @@ const genPrice = (package_data, fdFromDate, fdToDate) => {
         // $$('[data-sub-package]').forEach($el => {
         //     $el.setAttribute('data-sub-package', pack)
         // });
+
+        console.log("1",package_data[k]);
+        console.log("2",package_data[k].price[pack].price);
         $(`strong[data-price-${k}]`).innerHTML = parseInt(package_data[k].price[pack].price).toLocaleString();
         //$('#sub_code').value = pack;
 
