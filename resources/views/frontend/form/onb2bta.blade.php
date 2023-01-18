ddf7e3e2d97ae816e854567e139f391158382e9b
tuneprotect <yutthana.s@tuneprotect.com>
2023-01-18 16:17:49 +0700

    update banner rabbit

--- resources/views/frontend/form/onb2bta.blade.php
+++ resources/views/frontend/form/onb2bta.blade.php
@@ -54,7 +54,7 @@
                 </div>
                 <div class="controls-wrapper">
                     <input id="fdToDate" name="fdToDate" type="text" class="flatpickr"
-                           data-mindate="{{date('Y-m-d',strtotime( "+2 days"))}}"
+                           data-mindate="{{date('Y-m-d',strtotime( "+1 days"))}}"
                            data-error="@lang('product.error.travel_end_date')"/>
                     <label for="fdToDate">@lang("product.return_date")</label>
                 </div>
