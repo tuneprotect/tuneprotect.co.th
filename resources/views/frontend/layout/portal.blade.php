a9783bcec55427cfcafa736fadd73535c5a848cb
tuneprotect <yutthana.s@tuneprotect.com>
2023-01-18 16:25:05 +0700

    no message

--- resources/views/frontend/layout/portal.blade.php
+++ resources/views/frontend/layout/portal.blade.php
@@ -147,8 +147,8 @@
                                             </picture>
                                         @elseif($partner==='RABBIT')
                                             <picture>
-                                                <source media="(min-width:768px)" srcset="/storage/Banner/Banner_TA_Rabbit_D_TH.jpg">
-                                                <img src="{{url(!empty($v->pic_mobile_en) ? "/storage/Banner/Banner_TA_Rabbit_D_TH.jpg" : "/storage/Banner/Banner_TA_Rabbit_MB_TH.jpg" )}}"
+                                                <source media="(min-width:768px)" srcset="/storage/Banner/Banner_TA_Rabbit_D_EN.jpg">
+                                                <img src="{{url(!empty($v->pic_mobile_en) ? "/storage/Banner/Banner_TA_Rabbit_D_EN.jpg" : "/storage/Banner/Banner_TA_Rabbit_MB_EN.jpg" )}}"
                                                      alt="{{$v->locales[$locale]->title}}">
                                             </picture>
                                         @elseif($selected==='ONTALN')
@@ -263,6 +263,14 @@
                                         <img src="{{url(!empty($v->pic_mobile_en) ? "/storage/Banner/b2b/iSmile-Banner01.jpg" : "/storage/Banner/b2b/iSmile-Banner02.jpg" )}}"
                                                 alt="{{$v->locales[$locale]->title}}">
                                     </picture>
+                                @elseif(isset($partner))
+                                    @if($partner==='RABBIT')
+                                        <picture>
+                                            <source media="(min-width:768px)" srcset="/storage/Banner/Banner_TA_Rabbit_D_TH.jpg">
+                                            <img src="{{url(!empty($v->pic_mobile_en) ? "/storage/Banner/Banner_TA_Rabbit_D_TH.jpg" : "/storage/Banner/Banner_TA_Rabbit_MB_TH.jpg" )}}"
+                                                    alt="{{$v->locales[$locale]->title}}">
+                                        </picture>
+                                    @endif
                                 @else
                                     <picture>
                                         <source media="(min-width:768px)" srcset="{{url($v->pic)}}">
