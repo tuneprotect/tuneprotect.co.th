cb919d34f7af28af45a942884a5c58cbf1a51101
tuneprotect <yutthana.s@tuneprotect.com>
2023-01-24 11:10:39 +0700

    no message

--- resources/views/frontend/layout/portal.blade.php
+++ resources/views/frontend/layout/portal.blade.php
@@ -263,8 +263,8 @@
                                     </picture>
                                 @elseif($selected==='ONTADM')
                                     <picture>
-                                        <source media="(min-width:768px)" srcset="/storage/Banner/Banner_TG_D.jpg">
-                                        <img src="{{url(!empty($v->pic_mobile_en) ? "/storage/Banner/Banner_TG_D.jpg" : "/storage/Banner/Banner_TG_M.jpg" )}}"
+                                        <source media="(min-width:768px)" srcset="/storage/Banner/Banner_TA_D.jpg">
+                                        <img src="{{url(!empty($v->pic_mobile_en) ? "/storage/Banner/Banner_TA_D.jpg" : "/storage/Banner/Banner_TA_M.jpg" )}}"
                                                 alt="{{$v->locales[$locale]->title}}">
                                     </picture>
                                 @elseif($selected==='ONCOVIDMW')
