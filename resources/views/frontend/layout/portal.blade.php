60e92abd2801a901b31d07f04f04f5ffc90fc063
tuneprotect <yutthana.s@tuneprotect.com>
2023-02-01 08:34:08 +0700

    remove GT

--- resources/views/frontend/layout/portal.blade.php
+++ resources/views/frontend/layout/portal.blade.php
@@ -37,40 +37,7 @@
     </div>
 </header>
 @else
-<header>
-    @if($partner==='rabbit' && $selected==='ONTAOB')
-    <!-- Event snippet for Payment Completed conversion page -->
-    <script>
-        gtag('event', 'conversion', {
-            'send_to': 'AW-10813138401/MXLqCPPS3ocYEOHLjaQo',
-            'transaction_id': ''
-        });
-    </script>
-
-    <!-- Facebook Pixel Code -->
-    <script>
-        ! function(f, b, e, v, n, t, s) {
-            if (f.fbq) return;
-            n = f.fbq = function() {
-                n.callMethod ?
-                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-            };
-            if (!f._fbq) f._fbq = n;
-            n.push = n;
-            n.loaded = !0;
-            n.version = '2.0';
-            n.queue = [];
-            t = b.createElement(e);
-            t.async = !0;
-            t.src = v;
-            s = b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t, s)
-        }(window, document, 'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '682962723368236');
-        fbq('track', 'PageView');
-        fbq('track', 'PaymentCompleted');
-    </script>
+<header>    
     <noscript>
         <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=682962723368236&ev=PageView&noscript=1" />
     </noscript>
