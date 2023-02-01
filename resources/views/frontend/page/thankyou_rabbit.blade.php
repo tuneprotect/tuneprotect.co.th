0f9f59acfccc52dfe156e7e9731972ca6bd81294
tuneprotect <yutthana.s@tuneprotect.com>
2023-02-01 11:11:00 +0700

    edit thk page

--- resources/views/frontend/page/thankyou_rabbit.blade.php
+++ resources/views/frontend/page/thankyou_rabbit.blade.php
@@ -1,44 +1,68 @@
-@extends('frontend.layout.main')
+@extends('frontend.layout.portal')
 
 @section('page')
+
+<style>
+    header{
+        display:none;
+    }
+</style>
 <header>
-                @if($partner==='rabbit' && $selected==='ONTAOB')
-                   <!-- Event snippet for Payment Completed conversion page -->
-                    <script>
-                    gtag('event', 'conversion', {
-                        'send_to': 'AW-10813138401/MXLqCPPS3ocYEOHLjaQo',
-                        'transaction_id': ''
-                    });
-                    </script>
+        @if($partner==='rabbit' && $selected==='ONTAOB')
+            <!-- Event snippet for Payment Completed conversion page -->
+            <script>
+            gtag('event', 'conversion', {
+                'send_to': 'AW-10813138401/MXLqCPPS3ocYEOHLjaQo',
+                'transaction_id': ''
+            });
+            </script>
 
-                    <!-- Facebook Pixel Code -->
-                    <script>
-                    !function(f,b,e,v,n,t,s)
-                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                    n.queue=[];t=b.createElement(e);t.async=!0;
-                    t.src=v;s=b.getElementsByTagName(e)[0];
-                    s.parentNode.insertBefore(t,s)}(window, document,'script',
-                    'https://connect.facebook.net/en_US/fbevents.js');
-                    fbq('init', '682962723368236');
-                    fbq('track', 'PageView');
-                    fbq('track', 'PaymentCompleted');
-                    </script>
-                    <noscript>
-                    <img height="1" width="1" style="display:none" 
-                        src="https://www.facebook.com/tr?id=682962723368236&ev=PageView&noscript=1"/>
-                    </noscript>
-                    <!-- End Facebook Pixel Code -->
+            <!-- Facebook Pixel Code -->
+            <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '682962723368236');
+            fbq('track', 'PageView');
+            fbq('track', 'PaymentCompleted');
+            </script>
+            <noscript>
+            <img height="1" width="1" style="display:none" 
+                src="https://www.facebook.com/tr?id=682962723368236&ev=PageView&noscript=1"/>
+            </noscript>
+            <!-- End Facebook Pixel Code -->
 
-                @endif
-                    </header>
+        @endif
+    </header>
     <main>
         <article class="wrapper">
             <div class="inner-wrapper">
-                <div>{!! @str_replace('{point}',$point,@str_replace('{link}',$return_link,  @str_replace('{doc_no}'," ".$doc_no,$content->locales[$locale]->content)))   !!}</div>
+                <div>
+                    <p><img alt="" src="/storage/Icon/thankyou.jpg" /></p>
+                    <h1>Thank you for choosing Tune Protect</h1>
+                    <p>Your coverage starts now</p>
+                    <p>Please check your insurance policy following your specified channel. Your policy number for your<br />
+                        reference:{{$doc_no}}</p>
+                    <p><a class="btn btn-primary" href={{$return_link}}>Back to Main Page</a></p>
+                    <br>
+
+                </div>
+               
+                <div>
+                    
+                </div>
             </div>
+
+
         </article>
+
+
+
     </main>
 
 @endsection
