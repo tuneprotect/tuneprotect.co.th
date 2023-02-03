795436f6a9ae5478bb25948a1d7c017ae50fc100
tuneprotect <yutthana.s@tuneprotect.com>
2023-02-03 15:56:46 +0700

    edit text plan

--- resources/views/frontend/component/form-chill-sure-table.blade.php
+++ resources/views/frontend/component/form-chill-sure-table.blade.php
@@ -11,7 +11,7 @@
             <thead>
                 <tr>
                     <th>
-                        <h3>@lang('product.ci_coverage')</h3>
+                        <h3>@lang('product.chillsure_coverage')</h3>
                         <?php $i = 1 ?>
                         <div class="choose-plan-mobile">
 
