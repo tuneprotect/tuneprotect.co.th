2f269ea661058c0d7771ff353126d506315baee2
tuneprotect <yutthana.s@tuneprotect.com>
2023-02-09 10:32:20 +0700

    no message

--- resources/views/frontend/component/form-coverage-table-pumppa.blade.php
+++ resources/views/frontend/component/form-coverage-table-pumppa.blade.php
@@ -82,7 +82,7 @@
                     <th data-cover-{{$k}}>{!! $v !!}</th>
 
                     @foreach ($package_detail as $k1 => $v1)
-                        <td class="{{$k}} {{$i > 1 ? 'hide' : ''}}" data-index="{{$i}}" data-package="{{$k1}}">
+                        <td class="{{$k1}} {{$i > 1 ? 'hide' : ''}}" data-index="{{$i}}" data-package="{{$k1}}">
                             @if(isset($v1->plan->$k))
                                 @if((is_numeric($v1->plan->$k)))
                                     <strong>{{number_format( $v1->plan->$k,0)}}</strong>
