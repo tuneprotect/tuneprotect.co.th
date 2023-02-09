ef8d7fe390c755b41cb27a036b966b7ccde3c5c4
tuneprotect <yutthana.s@tuneprotect.com>
2023-02-09 09:40:30 +0700

    no message

--- resources/views/frontend/component/form-stepper-pumppa.blade.php
+++ resources/views/frontend/component/form-stepper-pumppa.blade.php
@@ -11,7 +11,7 @@
                 href="#"><strong>@lang('product.select_plan')</strong></a></li>
         <li id="step_3" data-gtm="product-pumppa-stepper-3" class="btn-goto" data-step="3"><a
                 href="#"><strong>@lang('product.fill_in_information')</strong></a></li>
-        <li id="step_4" data-gtm="product-{{strtolower($xxx)}}-stepper-4" class="btn-goto" data-step="4"><a
+        <li id="step_4" data-gtm="product-pumppa-stepper-4" class="btn-goto" data-step="4"><a
                 href="#">
                 <strong>
                     @lang('product.confirm')
