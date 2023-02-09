649870f3f87b40c8cd6af4758bf725526b8b6dea
tuneprotect <yutthana.s@tuneprotect.com>
2023-02-09 09:08:52 +0700

    no message

--- app/Http/Controllers/Frontend/PumpFreePAController.php
+++ app/Http/Controllers/Frontend/PumpFreePAController.php
@@ -25,8 +25,9 @@ class PumpFreePAController extends BaseController
     {
         $return_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
         session(['return_link' => $return_link]);
-        $selected = "Silver";
-        return $this->genView('frontend.page.pump_free_pa_silver');
+        //$selected = "Silver";
+        return redirect()->route('current', ['locale' => $this->locale, 'controller' => $this->controller, 'func' => $link, 'params' => $selected]);
+        //return $this->genView('frontend.page.pump_free_pa_silver');
     }
     public function Gold()
     {
