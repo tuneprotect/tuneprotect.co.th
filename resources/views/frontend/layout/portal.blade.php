6ae74aee7dad7cfd7953245e98baec1fd66e78fd
tuneprotect <yutthana.s@tuneprotect.com>
2023-02-13 16:48:03 +0700

    no message

--- resources/views/frontend/layout/portal.blade.php
+++ resources/views/frontend/layout/portal.blade.php
@@ -159,16 +159,16 @@
         <source media="(min-width:768px)" srcset="/storage/Banner/Banner_Luma_D.jpg">
         <img src="{{url(!empty($v->pic_mobile_en) ? "/storage/Banner/Banner_Luma_D.jpg" : "/storage/Banner/Banner_Luma_M.jpg" )}}" alt="{{$v->locales[$locale]->title}}">
     </picture>    
-    @elseif($selected==='TAIPOCT22')
-    <picture class="ccc">
-        <source media="(min-width:768px)" srcset="/storage/Banner/Tune_iPass_Desktop.jpg">
-        <img src="{{url(!empty($v->pic_mobile_en) ? "/storage/Banner/Tune_iPass_Desktop.jpg" : "/storage/Banner/Tune_iPass_Mobile.jpg" )}}" alt="{{$v->locales[$locale]->title}}">
-    </picture>
     @elseif(($agentCode==='00DM004D00') && ($selected==='TAIPOCT22'))
         <picture class="aa">
             <source media="(min-width:768px)" srcset="https://www.tuneprotect.co.th/storage/Banner/Tune iPass/dtacbanner.jpg">
             <img src="{{url(!empty($v->pic_mobile_en) ? "https://www.tuneprotect.co.th/storage/Banner/Tune iPass/dtacbanner.jpg" : "/storage/Banner/Banner_Luma_M.jpg" )}}" alt="{{$v->locales[$locale]->title}}">
         </picture>
+    @elseif($selected==='TAIPOCT22')
+    <picture class="ccc">
+        <source media="(min-width:768px)" srcset="/storage/Banner/Tune_iPass_Desktop.jpg">
+        <img src="{{url(!empty($v->pic_mobile_en) ? "/storage/Banner/Tune_iPass_Desktop.jpg" : "/storage/Banner/Tune_iPass_Mobile.jpg" )}}" alt="{{$v->locales[$locale]->title}}">
+    </picture>    
     @elseif($partner==='rabbit' && $selected==='ONTAOB')
     <picture>
         <source media="(min-width:768px)" srcset="/storage/Banner/Banner_TA_Rabbit_D_EN.jpg">
