6617abfb31cf7342179482f91bada9fc6622b69c
tuneprotect <yutthana.s@tuneprotect.com>
2023-02-13 16:42:12 +0700

    no message

--- app/Http/Controllers/Frontend/PortalController.php
+++ app/Http/Controllers/Frontend/PortalController.php
@@ -89,7 +89,7 @@ class PortalController extends ProductController
         session(['nopayment_status' => $nopayment_status]);
         session(['partner' => $partner]);
 
-        //echo var_dump($agentCode, );exit();
+        echo var_dump($agentCode, $massage_key);exit();
         return parent::index($link, $selected);
     }
 
