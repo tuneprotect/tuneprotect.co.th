f553af9c1dceb570f1697a6c8f6d7e88809013bb
tuneprotect <yutthana.s@tuneprotect.com>
2023-02-14 10:44:32 +0700

    add banner

--- resources/views/frontend/layout/portal.blade.php
+++ resources/views/frontend/layout/portal.blade.php
@@ -162,7 +162,7 @@
     @elseif($agentCode==='00DM004D00' && $selected==='TAIPOCT22')
         <picture class="aa">
             <source media="(min-width:768px)" srcset="https://www.tuneprotect.co.th/storage/Banner/Tune%20iPass/dtacbanner.jpg">
-            <img src="{{url(!empty($v->pic_mobile_en) ? "https://www.tuneprotect.co.th/storage/Banner/Tune%20iPass/dtacbanner.jpg" : "/storage/Banner/Banner_Luma_M.jpg" )}}" alt="{{$v->locales[$locale]->title}}">
+            <img src="{{url(!empty($v->pic_mobile_en) ? "https://www.tuneprotect.co.th/storage/Banner/Tune%20iPass/dtacbanner.jpg" : "https://www.tuneprotect.co.th/storage/Banner/Tune%20iPass/dtacbanner.jpg" )}}" alt="{{$v->locales[$locale]->title}}">
         </picture>
     @elseif($selected==='TAIPOCT22')
     <picture class="ccc">
