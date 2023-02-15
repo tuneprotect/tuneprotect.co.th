a28c41b4ebd89b06a395e41c3d494aa9f1735d29
tuneprotect <yutthana.s@tuneprotect.com>
2023-02-15 09:42:53 +0700

    no message

--- resources/views/frontend/layout/portal.blade.php
+++ resources/views/frontend/layout/portal.blade.php
@@ -10,7 +10,7 @@
 {{-- <meta name="csrf-token" content="{{ csrf_token() }}" />--}}
 
 <meta name="csrf-token" content="{{ csrf_token() }}" />
-@if(($partner==='LUMA' && ($selected==='ONTALN' || $selected ==='TAIPOCT22' || $selected ==='TAIPCRN')) || $selected==='TGCVLP' || $selected==='TAISMTG' || $selected==='TAIPCRN')
+@if(($partner==='LUMA' && ($selected==='ONTALN' || $selected ==='TAIPOCT22' || $selected ==='TAIPCRN')) || $selected==='TGCVLP' || $selected==='TAISMTG')
 @elseif($partner==='LUMA' && $selected==='CVCARE')
 <header>
     <nav>
