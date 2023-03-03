7b9db22200cf555890425f92d34d7d11e89159e7
tuneprotect <yutthana.s@tuneprotect.com>
2023-03-03 15:59:33 +0700

    no message

--- resources/views/frontend/form/pumppa.blade.php
+++ resources/views/frontend/form/pumppa.blade.php
@@ -1,8 +1,6 @@
 @include('frontend.component.form-stepper')
 <section id="step1" class="wrapper">
     @include('frontend.component.form-birthdate')
-    <br/>
-    {!! $package->locales[$locale]->remark !!}
     <div class="controls-wrapper full">
         <input id="ctrl_accept_step1" name="ctrl_accept_insurance_term" type="checkbox"
             data-error-insurance_term="@lang("product.error.insurance_term")"
@@ -10,6 +8,9 @@
         <label for="ctrl_accept_step1">@lang('product.ONCSHC_MSG.oncshc_msg_accept_step1')</label>
         <cite class="step1_error"></cite>
     </div>
+    <br/>
+    {!! $package->locales[$locale]->remark !!}
+   
 </section>
 @include('frontend.component.form-coverage-table-pumppa',['package_detail' => $package_detail,'selected' =>$selected ])
 
