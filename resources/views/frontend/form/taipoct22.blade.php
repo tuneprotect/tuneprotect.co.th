0c13240a39cc186c27783c9cb488662392b8f204
tuneprotect <yutthana.s@tuneprotect.com>
2023-03-13 14:01:17 +0700

    no message

--- resources/views/frontend/form/taipoct22.blade.php
+++ resources/views/frontend/form/taipoct22.blade.php
@@ -64,14 +64,15 @@
             <div>
                 <p class="small">@lang('product.ipass_warning')</p>
             </div>
+            <div class="controls-wrapper full">
+                <input id="ctrl_accept_step1" name="ctrl_accept_insurance_term" type="checkbox"
+                    data-error-insurance_term="@lang("product.error.insurance_term")"
+                    value="1"/>
+                <label for="ctrl_accept_step1">@lang('product.msg_accept_step1')</label>
+                <cite class="step1_error"></cite>
+            </div>
         </div>
-        <div class="controls-wrapper full">
-            <input id="ctrl_accept_step1" name="ctrl_accept_insurance_term" type="checkbox"
-                data-error-insurance_term="@lang("product.error.insurance_term")"
-                value="1"/>
-            <label for="ctrl_accept_step1">@lang('product.msg_accept_step1')</label>
-            <cite class="step1_error"></cite>
-        </div>
+        
         <div class="btn-wrapper">
             <button id="nextstep1" data-gtm="product-{{strtolower($selected)}}-proceed-step-1" data-step="2"
                     class="btn btn-primary btn-goto">@lang('product.next')</button>
