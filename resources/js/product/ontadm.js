b491a7d19192e638e32c4fe0443b367cc7043cc9
tuneprotect <yutthana.s@tuneprotect.com>
2023-03-13 13:49:47 +0700

    no message

--- resources/js/product/ontadm.js
+++ resources/js/product/ontadm.js
@@ -8,7 +8,7 @@ import {
 } from "../form/productHelper";
 import {$, $$, current_package, getRadioSelectedValue, getZipcodeData, locale, scrollToTargetAdjusted} from "../helper";
 
-import {removeError, showError, showFieldError, validateField} from "../validate_form";
+import {removeError, showError, showFieldError, validateField,validateAcceptStep1,showAcceptError} from "../validate_form";
 import Swal from "sweetalert2";
 import validate from "validate.js";
 import {differenceInDays, format, parseISO} from "date-fns";
@@ -353,6 +353,12 @@ document.addEventListener("DOMContentLoaded", async () => {
             } else {
                 switch (parseInt(step)) {
                     case 1:
+                        const chkAccept = validateAcceptStep1();
+                        if(!chkAccept){
+                            showAcceptError($('#ctrl_accept_step1').getAttribute('data-error-insurance_term'));
+                            status = false;
+                            break;
+                        }
                         status = true;
                         data = {
                             ...data,
--- resources/views/frontend/form/ontadm.blade.php
+++ resources/views/frontend/form/ontadm.blade.php
@@ -39,6 +39,13 @@
                     <label for="ctrl_no_of_insured">@lang("product.no_of_insured")</label>
                 </div>
             </div>
+            <div class="controls-wrapper full">
+                <input id="ctrl_accept_step1" name="ctrl_accept_insurance_term" type="checkbox"
+                    data-error-insurance_term="@lang("product.error.insurance_term")"
+                    value="1"/>
+                <label for="ctrl_accept_step1">@lang('product.msg_accept_step1')</label>
+                <cite class="step1_error"></cite>
+            </div>
         </div>
         <div class="btn-wrapper">
             <button data-gtm="product-{{strtolower($selected)}}-proceed-step-1" data-step="2" class="btn btn-primary btn-goto">@lang('product.next')</button>
