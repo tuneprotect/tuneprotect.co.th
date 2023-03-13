824b68a5cc6368c393ad62b2c2c72f0edbff70d3
tuneprotect <yutthana.s@tuneprotect.com>
2023-03-13 13:40:07 +0700

    no message

--- resources/js/validate_form.js
+++ resources/js/validate_form.js
@@ -41,11 +41,6 @@ export const validateField = ($this, constraints) => {
     }
 }
 export const validateAcceptStep1 = () => {
-    let chk = $('cite.step1_error').length;
-    if(chk==0){
-        return false;
-    }
-    $('cite.step1_error').innerHTML = "";
     let chkAccept = $('#ctrl_accept_step1').checked ? true : false;
     return chkAccept;
 }
