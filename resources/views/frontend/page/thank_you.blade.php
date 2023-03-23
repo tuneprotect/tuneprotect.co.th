31e058c7f055271d756c6a796440b05272357b97
tuneprotect <yutthana.s@tuneprotect.com>
2023-03-23 17:13:41 +0700

    no message

--- resources/views/frontend/page/thank_you.blade.php
+++ resources/views/frontend/page/thank_you.blade.php
@@ -41,8 +41,8 @@
 
     <main>
         <article class="wrapper">
-            <div class="inner-wrapper cc">			
-                <div>{!! @str_replace('{point}',$point,@str_replace('{link}',$return_link,  @str_replace('{doc_no}'," ".$doc_no,$content->locales[$locale]->content)))   !!}</div>
+            <div class="inner-wrapper">			
+                <div>{!! @str_replace('{point}',$point, @str_replace('{agentCode}',$agentCode, @str_replace('{payAmount}',$payAmount,@str_replace('{link}',$return_link,  @str_replace('{doc_no}'," ".$doc_no,$content->locales[$locale]->content)))   !!}</div>
             </div>
         </article>
     </main>
