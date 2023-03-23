6beb5efb1c7a69f663903968932dcdf498b1e233
tuneprotect <yutthana.s@tuneprotect.com>
2023-03-23 17:06:34 +0700

    no message

--- resources/views/frontend/page/thankyou.blade.php
+++ resources/views/frontend/page/thankyou.blade.php
@@ -3,7 +3,7 @@
 @section('page')
 <main>
     <article class="wrapper">
-        <div class="inner-wrapper">
+        <div class="inner-wrapper aaa">
             <div>{!! @str_replace('{point}',$point, @str_replace('{agentCode}',$agentCode, @str_replace('{payAmount}',$payAmount, @str_replace('{link}',$return_link, @str_replace('{doc_no}'," ".$doc_no,$content->locales[$locale]->content))))) !!}</div>
         </div>
     </article>
