9fae6d107aab931a055e94ba6821b6876977c6bd
tuneprotect <yutthana.s@tuneprotect.com>
2023-03-27 13:52:52 +0700

    no message

--- app/Http/Controllers/Api/ApiConnectController.php
+++ app/Http/Controllers/Api/ApiConnectController.php
@@ -74,7 +74,7 @@ class ApiConnectController extends BaseApiController
     }
     public function chkAirAsiaMemberID($memberID){
         header('Content-Type: application/json'); // Specify the type of data
-        $ch = curl_init('http://10.8.9.2:8002/api/WEBSITE/AirAsiaValidateMember'); // Initialise cURL
+        $ch = curl_init('http://10.8.9.2:8002/api/WEBSITE/blockHomePolicy'); // Initialise cURL
         //$post = json_encode($post); // Encode the data array into a JSON string
         $post = [
             'memberId' => '9999990026455651',
@@ -84,7 +84,7 @@ class ApiConnectController extends BaseApiController
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization )); // Inject the token into the header
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POST, 1); // Specify the request method as POST
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post); // Set the posted fields
+        //curl_setopt($ch, CURLOPT_POSTFIELDS, $post); // Set the posted fields
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); // This will follow any redirects
         $result = curl_exec($ch); // Execute the cURL statement
         curl_close($ch); // Close the cURL connection
