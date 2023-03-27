df50ae976192b51722f43460cc1e7e3834151703
tuneprotect <yutthana.s@tuneprotect.com>
2023-03-27 16:05:22 +0700

    no message

--- resources/views/frontend/layout/portal.blade.php
+++ resources/views/frontend/layout/portal.blade.php
@@ -138,8 +138,8 @@
                 </script>
             @else
                 <picture>
-                    <source media="(min-width:768px)" srcset="https://www.tuneprotect.co.th/storage/Banner/chillsure/Tune-Chillsure-02.webp">
-                    <img src="{{url(!empty($v->pic_mobile_en) ? "https://www.tuneprotect.co.th/storage/Banner/chillsure/Tune-Chillsure-04.webp" : "https://www.tuneprotect.co.th/storage/Banner/chillsure/Tune-Chillsure-04.webp" )}}" alt="{{$v->locales[$locale]->title}}">
+                    <source media="(min-width:768px)" srcset="https://www.tuneprotect.co.th/storage/Banner/Banner-chillsure-main-en-pc.jpg">
+                    <img src="{{url(!empty($v->pic_mobile_en) ? "https://www.tuneprotect.co.th/storage/Banner/Banner-chillsure-main-en-mb.jpg" : "https://www.tuneprotect.co.th/storage/Banner/Banner-chillsure-main-en-mb.jpg" )}}" alt="{{$v->locales[$locale]->title}}">
                 </picture>
                 
             @endif   
@@ -301,8 +301,8 @@
                 </script>
             @else
                 <picture>
-                    <source media="(min-width:768px)" srcset="https://www.tuneprotect.co.th/storage/Banner/chillsure/Tune-Chillsure-02.webp">
-                    <img src="{{url(!empty($v->pic_mobile_en) ? "https://www.tuneprotect.co.th/storage/Banner/chillsure/Tune-Chillsure-04.webp" : "https://www.tuneprotect.co.th/storage/Banner/chillsure/Tune-Chillsure-04.webp" )}}" alt="{{$v->locales[$locale]->title}}">
+                    <source media="(min-width:768px)" srcset="https://www.tuneprotect.co.th/storage/Banner/Banner-chillsure-main-th-pc.jpg">
+                    <img src="{{url(!empty($v->pic_mobile_en) ? "https://www.tuneprotect.co.th/storage/Banner/Banner-chillsure-main-th-mb.jpg" : "https://www.tuneprotect.co.th/storage/Banner/Banner-chillsure-main-th-mb.jpg" )}}" alt="{{$v->locales[$locale]->title}}">
                 </picture>               
             @endif   
         @endif    
