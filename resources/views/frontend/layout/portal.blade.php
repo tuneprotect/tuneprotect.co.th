2727982e17ccc2bbddc81f997f282e8c42db7cac
tuneprotect <kriankrai.t@tuneprotect.co.th>
2023-04-17 15:09:19 +0700

    Print selected to example

--- resources/views/frontend/layout/portal.blade.php
+++ resources/views/frontend/layout/portal.blade.php
@@ -11,6 +11,7 @@
 {{-- <meta name="csrf-token" content="{{ csrf_token() }}" />--}}
 
 <meta name="csrf-token" content="{{ csrf_token() }}" />
+
 @if(($partner==='LUMA' && ($selected==='ONTALN' || $selected ==='TAIPOCT22' || $selected ==='TAIPOCT22AA' || $selected ==='TAIPCRN')) || $selected==='TGCVLP' || $selected==='TAISMTG')
 @elseif($partner==='LUMA' && $selected==='CVCARE')
 <header>
@@ -150,11 +151,6 @@
         <source media="(min-width:768px)" srcset="/storage/Banner/Banner_TA_D_EN.jpg">
         <img src="{{url(!empty($v->pic_mobile_en) ? "/storage/Banner/Banner_TA_D_EN.jpg" : "/storage/Banner/Banner_TA_M_EN.jpg" )}}" alt="{{$v->locales[$locale]->title}}">
     </picture>
-    @elseif($selected==='ONTASK')
-    <picture>
-        <source media="(min-width:768px)" srcset="/storage/Banner/Banner_TA_D_EN.jpg">
-        <img src="{{url(!empty($v->pic_mobile_en) ? "/storage/Banner/Banner_TA_D_EN.jpg" : "/storage/Banner/Banner_TA_M_EN.jpg" )}}" alt="{{$v->locales[$locale]->title}}">
-    </picture>
     @elseif($selected==='ONCOVIDMW')
     <picture>
         <source media="(min-width:768px)" srcset="{{url(str_replace('Banner_iPass_COVID', 'Migrant-Banner-En01', $v->pic_en))}}">
@@ -219,6 +215,13 @@
         <source media="(min-width:768px)" srcset="{{url(str_replace('Banner_TA_D_EN', 'Banner_Tune_iPass', $v->pic_en))}}">
         <img src="{{url(!empty($v->pic_mobile_en) ? url(str_replace('Banner_TA_D_EN', 'Banner_Tune_iPass', $v->pic_mobile_en)) : url(str_replace('Banner_TA_D_EN', 'Banner_Tune_iPass', $v->pic_en)) )}}" alt="{{$v->locales[$locale]->title}}">
     </picture>
+    @else
+    <picture>
+        <source media="(min-width:768px)" srcset="{{url($v->pic_en)}}">
+        <img src="{{url(!empty($v->pic_mobile_en) ? $v->pic_mobile_en : $v->pic_en )}}" alt="{{$v->locales[$locale]->title}}">
+    </picture>
+    @endif
+
     @elseif($selected==='CVCARE')
     <picture>
         <source media="(min-width:768px)" srcset="/storage/Banner/Banner_LumaCare_D.jpg">
@@ -264,11 +267,6 @@
         <img src="{{url(!empty($v->pic_mobile) ? url(str_replace('Banner_Covid_D', 'Banner_VSure_Protect_D', $v->pic_mobile)) : url(str_replace('Banner_Covid_D', 'Banner_VSure_Protect_M', $v->pic)) )}}" alt="{{$v->locales[$locale]->title}}">
     </picture>
     @elseif($selected==='ONTADM')
-    <picture>
-        <source media="(min-width:768px)" srcset="/storage/Banner/Banner_TA_D.jpg">
-        <img src="{{url(!empty($v->pic_mobile_en) ? "/storage/Banner/Banner_TA_D.jpg" : "/storage/Banner/Banner_TA_M.jpg" )}}" alt="{{$v->locales[$locale]->title}}">
-    </picture>
-    @elseif($selected==='ONTASK')
     <picture>
         <source media="(min-width:768px)" srcset="/storage/Banner/Banner_TA_D.jpg">
         <img src="{{url(!empty($v->pic_mobile_en) ? "/storage/Banner/Banner_TA_D.jpg" : "/storage/Banner/Banner_TA_M.jpg" )}}" alt="{{$v->locales[$locale]->title}}">
