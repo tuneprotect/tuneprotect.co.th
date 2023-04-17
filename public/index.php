115f338f87f80d93f5e6c39d9557dc9577a2a81a
tuneprotect <yutthana.s@tuneprotect.com>
2023-04-17 14:55:37 +0700

    from source code p'dum

--- public/index.php
+++ public/index.php
@@ -49,6 +49,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 $kernel = $app->make(Kernel::class);
 
 $response = tap($kernel->handle(
+   
     $request = Request::capture()
 ))->send();
 
