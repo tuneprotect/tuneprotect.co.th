3c64ff6f7b173adcc57a6994ef07a6325988a592
tuneprotect <kriankrai.t@tuneprotect.co.th>
2023-04-21 11:05:42 +0700

    Reverse config follow env file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -50,14 +50,10 @@ $app->singleton(
 |-----------------------------------------------
 */
 
-if (isset($_SERVER['HTTP_HOST']) && !empty($_SERVER['HTTP_HOST'])) {
+if(isset($_SERVER['HTTP_HOST']) && !empty($_SERVER['HTTP_HOST'])){
     $domain = $_SERVER['HTTP_HOST'];
     if (isset($domain)) {
-        $dotenv = Dotenv\Dotenv::createImmutable(base_path(), '.env.' . $domain);
-        if ($domain === 'staging.webtest2.tuneinsurance.co.th') {
-            $dotenv = Dotenv\Dotenv::createImmutable(base_path(), '.env.staging.tuneprotect.co.th');
-        }
-
+        $dotenv = Dotenv\Dotenv::createImmutable(base_path(), '.env.'.$domain);
         try {
             $dotenv->load();
         } catch (InvalidPathException $e) {
