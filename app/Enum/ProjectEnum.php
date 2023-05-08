184d62de2940e4e4f6ca739e2051f018b0bc8630
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2023-05-08 14:19:40 +0700

    add thank you page #7

--- app/Enum/ProjectEnum.php
+++ app/Enum/ProjectEnum.php
@@ -36,15 +36,6 @@ class ProjectEnum
     const STATIC_PAGE_PAYMENT_CHILL_SURE_PENDING = 'static.page.payment_chill_sure_pending';
     const STATIC_PAGE_PAYMENT_CHILL_SURE_REJECT = 'static.page.payment_chill_sure_reject';
 
-    //Health
-    const STATIC_PAGE_PAYMENT_THANK_YOU_CHILL_SURE = 'static.page.payment_thank_you_chill_sure';
-    const STATIC_PAGE_PAYMENT_THANK_YOU_MYFLEXI_CI = 'static.page.payment_thank_you_myflexi_ci';
-    const STATIC_PAGE_PAYMENT_THANK_YOU_DIABETES = 'static.page.payment_thank_you_diabetes';
-
-    const STATIC_PAGE_PAYMENT_CHILL_SURE_ERROR = 'static.page.payment_chill_sure_error';
-    const STATIC_PAGE_PAYMENT_CHILL_SURE_CANCEL = 'static.page.payment_chill_sure_cancel';
-    const STATIC_PAGE_PAYMENT_CHILL_SURE_PENDING = 'static.page.payment_chill_sure_pending';
-    const STATIC_PAGE_PAYMENT_CHILL_SURE_REJECT = 'static.page.payment_chill_sure_reject';
 
     const STATIC_PAGE_PAYMENT_ERROR = 'static.page.payment_error';
     const STATIC_PAGE_PAYMENT_CANCEL = 'static.page.payment_cancel';
