2f55ac35fc73e5afd43fdcf4d908a88ada63da54
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2023-05-08 16:51:29 +0700

    add thank you page #18

--- app/Http/Controllers/Frontend/ProductController.php
+++ app/Http/Controllers/Frontend/ProductController.php
@@ -1118,7 +1118,7 @@ class ProductController extends BaseController
 
     public function error(Request $request)
     {
-        dd($request->session()->get('package'));
+        dd($request);
         if (substr($request->session()->get('package'), 0, 6) === ProjectEnum::ONCSHC_URL) {
             $this->bodyData['doc_no'] = $request->session()->get('error');
             return $this->genStatusPage(ProjectEnum::STATIC_PAGE_PAYMENT_ERROR_CHILL_SURE);
