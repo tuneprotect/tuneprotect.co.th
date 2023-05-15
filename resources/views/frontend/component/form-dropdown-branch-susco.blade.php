122e38eb8bff27db7dfe4eefdeb1ff9af951a31c
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2023-05-15 15:18:03 +0700

    add microsite susco #6

---
+++ resources/views/frontend/component/form-dropdown-branch-susco.blade.php
@@ -0,0 +1,20 @@
+<strong>@lang('product.branch_susco_title')</strong>
+<div class="date-input airasia-member-input">
+    <strong data-your-details="@lang('product.your-details')" data-loved-one="@lang('product.loved-one')"></strong>
+    <div class="date-wrapper">
+        <div class="controls-wrapper">
+            <input type="hidden" id="hdfMemberStatus" />
+            <select id="{{$prefix}}ctrl_province" name="{{$prefix}}ctrl_province"
+                data-please-select="@lang("global.default_select_option")"
+                data-error-province="@lang("product.error.province")">
+            </select>
+            <label for="fdMemberID">@lang("product.member_id")</label>
+            <cite class="bmi_error"></cite>
+        </div>
+        <div class="controls-wrapper"></div>
+        <div class="controls-wrapper"></div>
+    </div>
+</div>
+
+
+	
