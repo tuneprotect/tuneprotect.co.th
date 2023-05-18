0e383a199c83af473ed80ebb0203071cb477235f
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2023-05-18 09:42:00 +0700

    add page preference center #2

--- app/Http/Controllers/Frontend/PreferenceCenterController.php
+++ app/Http/Controllers/Frontend/PreferenceCenterController.php
@@ -3,11 +3,9 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use App\Enum\ProjectEnum;
 use App\Http\Controllers\Frontend\Base\BaseController;
-use App\Models\WebContent;
 
-class IndexController extends BaseController
+class PreferenceCenterController extends BaseController
 {
     public function index()
     {
