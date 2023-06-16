b5c05a81e4a679b83107233ff0e2175bbb265ca3
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2023-06-16 16:54:29 +0700

    fixed issue portal redirect product #10

--- app/Http/Controllers/Frontend/ProductController.php
+++ app/Http/Controllers/Frontend/ProductController.php
@@ -121,7 +121,7 @@ class ProductController extends BaseController
         $product_agent_code = $selected . '/' . $portal_key;
         $apiResult = $this->sendToApiPortalLogin($portal_key);
         if ($apiResult["status"]) {
-            return redirect()->route('current', ['locale' => $this->locale, 'controller' => 'portal', 'func' => $link, 'params' => $product_agent_code]);
+            return redirect()->route('current', ['locale' => $this->locale, 'controller' => 'portal/form', 'func' => $link, 'params' => $product_agent_code]);
         }
 
         if (in_array($selected, ['ONTALN','TAIPCRN', 'TAIPOCT22', 'TAIPOCT22AA', 'ONCOVIDL', 'ONTA', 'TGCVLP', 'TAISM', 'TAISMC', 'ONTAISMB2B', 'ONTGISM', 'TAISMTG']) && $this->locale === 'th') {
