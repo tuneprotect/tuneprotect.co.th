7bba68992cd14170d9d48a412e86820bfe0380e1
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2023-06-16 08:22:17 +0700

    fixed issue product list #1

--- resources/views/frontend/component/product_list.blade.php
+++ resources/views/frontend/component/product_list.blade.php
@@ -4,8 +4,7 @@
             <h1>{{$product->locales[$locale]->title}}</h1>
             <div class="item-wrapper">
                 @foreach ($product->productPackage as $v)
-                    @if($v->code === 'ONVACINA' || $v->code === 'CVCARE' || $v->code === 'ONCOVIDMW' || $v->code === 'TGCVLP' || $v->code === 'ONTALN' || $v->code === 'TAISMTG' || $v->code === 'ONVSUREA' || $v->code === 'ONTAOBB2B' || $v->code === 'ONB2BTA' || $v->code === 'ONB2BTAD' || $v->code === 'ONTAISMB2B' || $v->code === 'ONCSH' || $v->code === 'TAIPCRN' || $v->code === 'TAIPOCT22AA')
-                    @else
+                @if($v->code === 'ONCSHC' || $v->code === 'CI'|| $v->code === 'DIABETES' || $v->code === 'ONPACA' || $v->code === 'ONPAKD' || $v->code === 'ONPASN' || $v->code === 'ONTASK' || $v->code === 'TAISM' || $v->code === 'TAIPOCT22' || $v->code === 'ONTADM' || $v->code === 'ONTAOB' || $v->code === 'ONFIMP' || $v->code === 'ONMHS')
                         <div class="item">
                             <div class="two-tone-icon">
                                 <span><img src="{{url($v->pic)}}" alt=""></span>
@@ -21,8 +20,8 @@
                                     @endif
 
                                     <a href="{{route('current',['locale' => $locale,'controller' => 'product','func' => $product->friendly_url,'params' => $v->code ])}}"
-                                       data-gtm="all-product-{{strtolower($product->friendly_url)}}-{{$v->code}}"
-                                       class="btn btn-block btn-outline">@lang('global.choose_product')</a>
+                                        data-gtm="all-product-{{strtolower($product->friendly_url)}}-{{$v->code}}"
+                                        class="btn btn-block btn-outline">@lang('global.choose_product')</a>
 
                                 @else
                                     @if($controller == 'portal')
