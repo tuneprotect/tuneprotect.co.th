afac043ba38104fe90b20927c4d686430a020ff4
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2023-06-22 09:58:36 +0700

    update error terms and conditions #2

--- resources/views/frontend/component/form-privacy.blade.php
+++ resources/views/frontend/component/form-privacy.blade.php
@@ -24,7 +24,7 @@
 
 <div class="controls-wrapper full">
     <input id="ctrl_accept_insurance_term" name="ctrl_accept_insurance_term" type="checkbox" value="1"
-           data-error-insurance_term="@lang("product.error.insurance_term")"/>
+           data-error-insurance_term="@lang("product.error.insurance_term")" />
     <label for="ctrl_accept_insurance_term">@lang('product.accept_insurance')</label>
 </div>
 <p class="small">
