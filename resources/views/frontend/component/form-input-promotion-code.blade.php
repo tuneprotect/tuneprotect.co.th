f2730b1bb41919bb71003627fc91aabc5efa9b92
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2023-06-23 10:21:35 +0700

    update promotion code #6

--- resources/views/frontend/component/form-input-promotion-code.blade.php
+++ resources/views/frontend/component/form-input-promotion-code.blade.php
@@ -1,17 +1,15 @@
 <strong>@lang('product.PromotionCode.promotion_code_title')</strong>
 <div class="two-col">
     <strong data-your-details="@lang('product.your-details')" data-loved-one="@lang('product.loved-one')"></strong>
-    <div class="date-wrapper">
-        <div class="controls-wrapper">
-            <input type="hidden" id="hdfdPromotionCodeStatus" />
-            <input id="fdPromotionCode" type="text" name="fdPromotionCode" placeholder="@lang('product.PromotionCode.promotion_code')" 
-                   data-error-promotion-code-not-qualify="@lang('product.PromotionCode.promotion_code_error')"
-            />
-            <label for="fdPromotionCode">@lang("product.PromotionCode.promotion_code")</label>
-            <cite class="cite_error"></cite>
-        </div>
-        <div class="controls-wrapper"></div>
+    <div class="controls-wrapper">
+        <input type="hidden" id="hdfdPromotionCodeStatus" />
+        <input id="fdPromotionCode" type="text" name="fdPromotionCode" placeholder="@lang('product.PromotionCode.promotion_code')" 
+                data-error-promotion-code-not-qualify="@lang('product.PromotionCode.promotion_code_error')"
+        />
+        <label for="fdPromotionCode">@lang("product.PromotionCode.promotion_code")</label>
+        <cite class="cite_error"></cite>
     </div>
+    <div class="controls-wrapper"></div>
 </div>
 
 
