dad9258ef076f5f80f486d1c992853402771eb15
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2023-06-26 15:28:26 +0700

    fixed itravel rabbit #4

--- resources/views/frontend/component/form-input-promotion-code.blade.php
+++ resources/views/frontend/component/form-input-promotion-code.blade.php
@@ -1,7 +1,6 @@
 <strong>@lang('product.PromotionCode.promotion_code_title')</strong>
 <div class="two-col">
     <div class="controls-wrapper promotion-code">
-        <!-- <input type="hidden" id="hdfdPromotionCodeStatus" /> -->
         <input id="fdPromotionCode" type="text" name="fdPromotionCode" placeholder="@lang('product.PromotionCode.promotion_code')" 
             data-error-promotion-code-valid="@lang('product.PromotionCode.promotion_code_valid')"
             data-error-promotion-code-invalid="@lang('product.PromotionCode.promotion_code_invalid')"
