5b521350afdc40d7139cbd82c235bc94dbe99236
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2023-07-03 15:44:03 +0700

    update promotion code #62

--- resources/js/product/ontaob.js
+++ resources/js/product/ontaob.js
@@ -836,10 +836,11 @@ document.addEventListener("DOMContentLoaded", async () => {
                                 //     })
                                 // }
 
-                                `<div class="controls-wrapper full no-lable"><span>${$('#lblfdPromotionCode').innerText} : </span><strong>ส่วนลดสตาร์บัค มูลค่า 100 บาท</strong> ${ promotion_data.count < i+1 ? `<span id="promotion_code_alert" class="error" style="display: none;">(* The code has already been used.)</span>` : `""`  } </div>`
+                                sb += `<div class="controls-wrapper full no-lable"><span>${$('#lblfdPromotionCode').innerText} : </span><strong>ส่วนลดสตาร์บัค มูลค่า 100 บาท</strong> ${ promotion_data.count < i+1 ? 
+                                sb += `<span id="promotion_code_alert" class="error" style="display: none;">(* The code has already been used.)</span>` : ""  } </div>`
                             }
 
-                        `</div>`
+                        sb += `</div>`
 
                         });
 
