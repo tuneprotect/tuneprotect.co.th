5abd857238532f4da471bc88d5649bd86c1c9079
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2023-07-07 16:48:35 +0700

    update promotion code #82

--- resources/js/form/productHelper.js
+++ resources/js/form/productHelper.js
@@ -213,7 +213,7 @@ export const getSuscoBranch = async ($this) => {
     return callSuscoBranch();
 }
 
-export const validatePromotionCode = async ($this, promotionCode, costAmount) => {
+export const validatePromotionCode = async (promotionCode, costAmount) => {
     let data = {code: promotionCode, costAmount: costAmount}
     console.log(data);
     return callPromotionCode(data);
