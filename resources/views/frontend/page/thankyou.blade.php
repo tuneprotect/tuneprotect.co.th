b6c332c9517851ce5f7790a82c6c313553ac3b9d
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2023-08-28 16:16:22 +0700

    update thank you promotion code itravel

--- resources/views/frontend/page/thankyou.blade.php
+++ resources/views/frontend/page/thankyou.blade.php
@@ -179,6 +179,9 @@
     <article class="wrapper">
         <div class="inner-wrapper">
             <div>{!! @str_replace('{point}',$point, @str_replace('{agentCode}',$agentCode, @str_replace('{payAmount}',$payAmount, @str_replace('{link}',$return_link, @str_replace('{doc_no}'," ".$doc_no,$content->locales[$locale]->content))))) !!}</div>
+            @if(session()->get('thankyou_param') === 'ONTAOB')
+                @lang('product.PromotionCode.promotion_code_title')
+            @endif
         </div>
     </article>
 </main>
