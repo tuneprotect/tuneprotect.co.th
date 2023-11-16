eab395175021effa23e8c869a047b256b50f4837
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2023-11-16 14:22:45 +0700

    calculate age #5

--- resources/js/helper.js
+++ resources/js/helper.js
@@ -12,7 +12,7 @@ export const current_package = $('main').getAttribute('data-package');
 export const locale = $('html').getAttribute('lang');
 
 export const calculateAge = (date) => {
-    const now = new Date();
+    const now = new Date().addDays(1);
     let result = {
         year: 0,
         month: 0,
