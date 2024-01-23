35d8092d584af38c758186083c3a4b6161db26b6
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2024-01-23 14:32:25 +0700

    add on question for pa #10

--- resources/views/frontend/component/form-qustion-pa.blade.php
+++ resources/views/frontend/component/form-qustion-pa.blade.php
@@ -1,5 +1,5 @@
 <h3>@lang('product.another_company')</h3>
-<div class="controls-wrapper">
+<div class="">
     <ul class="check-wrapper">
         <li>
             <input id="ctrl_is_another_company_no" name="fdIsAnotherCompany" type="radio" checked value="N"/>
@@ -11,15 +11,21 @@
         </li>
     </ul>
     <div class="two-col another_company_name" style="display: none">
-        <div class="question-wrapper">
-            <p>@lang('product.another_policy')</p>
-            <input id="fdAnotherCompanyName1" name="fdAnotherCompanyName1" 
-                    type="text" placeholder="@lang('product.another_company_name')"
-                    data-error-tax_no-require="@lang("product.error.tax_no.require")"
-                    data-error-tax_no-format="@lang("product.error.tax_no.format")"
-            />
+        <div class="date-wrapper">
+            <div class="controls-wrapper">
+                <p>@lang('product.another_policy')</p>
+            </div>
+            <div class="controls-wrapper">
+                <input id="fdAnotherCompanyName1" name="fdAnotherCompanyName1" 
+                        type="text" placeholder="@lang('product.another_company_name')"
+                        data-error-tax_no-require="@lang("product.error.tax_no.require")"
+                        data-error-tax_no-format="@lang("product.error.tax_no.format")"
+                />
+            </div>
+            <div class="controls-wrapper">
+                <p>@lang('product.policy_unit')</p>
+            </div>
             <!-- <label for="fdTaxno">@lang('product.tax_no')</label> -->
-            <p>@lang('product.policy_unit')</p>
         </div>
         <div class="question-wrapper">
             <p>@lang('product.another_policy_price')</p>
