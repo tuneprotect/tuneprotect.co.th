535a9d44b5464e590648bcc9a2b186e893391389
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2024-01-25 09:56:42 +0700

    add on question for pa #42

--- resources/views/frontend/component/form-qustion-pa.blade.php
+++ resources/views/frontend/component/form-qustion-pa.blade.php
@@ -37,7 +37,7 @@
         </div>
         <div class="date-wrapper">
             <div><p class="small">@lang('product.company')</p></div>
-            <div><input id="fdAnotherCompName1" name="fdAnotherCompName1" 
+            <div class="controls-wrapper"><input id="fdAnotherCompName1" name="fdAnotherCompName1" 
                     type="text" data-error-another_company-format="@lang("product.error.another_company.format")" /></div>
             <div><p class="small">@lang('product.policy_price')</p></div>
             <div class="controls-wrapper">
@@ -49,7 +49,7 @@
         </div>
         <div class="date-wrapper">
             <div><p class="small">@lang('product.company')</p></div>
-            <div><input id="fdAnotherCompName2" name="fdAnotherCompName2" 
+            <div class="controls-wrapper"><input id="fdAnotherCompName2" name="fdAnotherCompName2" 
                     type="text" data-error-another_company-format="@lang("product.error.another_company.format")" /></div>
             <div><p class="small">@lang('product.policy_price')</p></div>
             <div class="controls-wrapper">
@@ -61,7 +61,7 @@
         </div>
         <div class="date-wrapper">
             <div><p class="small">@lang('product.company')</p></div>
-            <div><input id="fdAnotherCompName3" name="fdAnotherCompName3" 
+            <div class="controls-wrapper"><input id="fdAnotherCompName3" name="fdAnotherCompName3" 
                     type="text" data-error-another_company-format="@lang("product.error.another_company.format")" /></div>
             <div><p class="small">@lang('product.policy_price')</p></div>
             <div class="controls-wrapper">
