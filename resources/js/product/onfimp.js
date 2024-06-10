7395ccd8deed8e421a27b8077e04b8f1b2480b50
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2024-06-10 11:02:09 +0700

    fix data layer tag myhome #7

--- resources/js/product/onfimp.js
+++ resources/js/product/onfimp.js
@@ -302,29 +302,56 @@ const genItemList = (package_data, packageSelect) => {
     let index = 0;
     const itemList = [];
 
-    const allPack = Object.keys(package_data)
-            .filter(k => _.startsWith(k,packageSelect))
+    if (package_data) {
+        Object.keys(package_data)
+            .filter(k => _.startsWith(k, packageSelect))
+            .map(k => {
+                
+                const price = parseInt(package_data[k].price).toLocaleString();
+                const planCode = Object.keys(package_data)[index];
 
-    $$('#table-detail td[data-package],#table-detail th[data-package]').forEach($el => {
-        if (allPack.includes($el.getAttribute("data-package"))) {
+                const item = {
+                    item_id: "",
+                    item_name: "",
+                    item_brand: "",
+                    item_category: "",
+                    price: "",
+                };
+
+                item.item_id = "myHomePlus_" + planCode;
+                item.item_name = "myHome Plus Plan Code " + planCode;
+                item.item_brand = "myHome Plus";
+                item.item_category = "Fire Insurance";
+                item.price = price;
+
+                itemList.push(item);
+                index++;
+            });
+    }
+
+    // const allPack = Object.keys(package_data)
+    //         .filter(k => _.startsWith(k,packageSelect))
+
+    // $$('#table-detail td[data-package],#table-detail th[data-package]').forEach($el => {
+    //     if (allPack.includes($el.getAttribute("data-package"))) {
             
-            const item = {
-                item_id: "",
-                item_name: "",
-                item_brand: "",
-                item_category: "",
-                price: "",
-            };
+    //         const item = {
+    //             item_id: "",
+    //             item_name: "",
+    //             item_brand: "",
+    //             item_category: "",
+    //             price: "",
+    //         };
 
-            item.item_id = "myHomePlus_" + $el.getAttribute("data-package");
-            item.item_name = "myHome Plus Plan Code " + $el.getAttribute("data-package");
-            item.item_brand = "myHome Plus";
-            item.item_category = "Fire Insurance";
-            item.price = $el.getAttribute("data-price-" + $el.getAttribute("data-package"));
+    //         item.item_id = "myHomePlus_" + $el.getAttribute("data-package");
+    //         item.item_name = "myHome Plus Plan Code " + $el.getAttribute("data-package");
+    //         item.item_brand = "myHome Plus";
+    //         item.item_category = "Fire Insurance";
+    //         item.price = $el.getAttribute("data-price-" + $el.getAttribute("data-package"));
 
-            itemList.push(item);
-        }
-    });
+    //         itemList.push(item);
+    //     }
+    // });
 
     if ($('#controller').value === 'product') 
     {
