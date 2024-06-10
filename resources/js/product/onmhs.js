6b98e6fb6984a1e618aeb4a7d740e5741c7b571c
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2024-06-10 15:25:06 +0700

    fix data layer tag myhome #17

--- resources/js/product/onmhs.js
+++ resources/js/product/onmhs.js
@@ -302,32 +302,23 @@ const genItemList = (package_data) => {
     let index = 0;
     const itemList = [];
 
-    if (package_data) {
-        Object.keys(package_data)
-            .filter(k => _.startsWith(k, current_package))
-            .map(k => {
-                
-                const price = parseInt(package_data[k].price).toLocaleString();
-                const planCode = Object.keys(package_data)[index];
+    $$('#table-detail tfoot tr td[data-package]').forEach($el => {
+        const item = {
+            item_id: "",
+            item_name: "",
+            item_brand: "",
+            item_category: "",
+            price: "",
+        };
 
-                const item = {
-                    item_id: "",
-                    item_name: "",
-                    item_brand: "",
-                    item_category: "",
-                    price: "",
-                };
+        item.item_id = "myHomeSmart_" + $el.getAttribute("data-package");
+        item.item_name = "myHome Smart Plan Code " + $el.getAttribute("data-package");
+        item.item_brand = "myHome Smart";
+        item.item_category = "Fire Insurance";
+        item.price = $el.getElementsByTagName("input")[0].value;
 
-                item.item_id = "myHomeSmart_" + planCode;
-                item.item_name = "myHome Smart Plan Code " + planCode;
-                item.item_brand = "myHome Smart";
-                item.item_category = "Fire Insurance";
-                item.price = price;
-
-                itemList.push(item);
-                index++;
-            });
-    }
+        itemList.push(item);
+    });
 
     if ($('#controller').value === 'product') 
     {
@@ -340,7 +331,6 @@ const genItemList = (package_data) => {
             }
         });
     }
-    
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
