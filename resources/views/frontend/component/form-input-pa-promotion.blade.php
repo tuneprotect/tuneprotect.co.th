88eb8902e67f2ba83019e86c2aec0e395fc2fc93
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2024-07-08 16:44:34 +0700

    pa promotion #3

--- resources/views/frontend/component/form-input-pa-promotion.blade.php
+++ resources/views/frontend/component/form-input-pa-promotion.blade.php
@@ -1,24 +1,20 @@
 <strong>@lang('product.PromotionCode.promotion_code_title')</strong>
-<div class="two-col">
-    <div class="controls-wrapper promotion-code">
-        <div class="grid">
-            <label class="card">
-                <input name="plan" class="radio" type="radio" checked>
-                <span class="plan-details">
-                <img src="/storage/promotion/Starbucks_Giftcard.png" />
-                </span>
-            </label>
-            <label class="card">
-                <input name="plan" class="radio" type="radio">
-                <span class="plan-details" aria-hidden="true">
-                <img src="/storage/promotion/Central_Giftcard.png" />
-                </span>
-            </label>
-        </div>
-        <label id="lblfdPromotionCode" for="fdPromotionCode">@lang("product.PromotionCode.promotion_code")</label>
-        <span class="span_error"></span>
+<div class="promotion-code">
+    <div class="grid">
+        <label class="card">
+            <input name="plan" class="radio" type="radio" checked>
+            <span class="plan-details">
+            <img src="/storage/promotion/Starbucks_Giftcard.png" />
+            </span>
+        </label>
+        <label class="card">
+            <input name="plan" class="radio" type="radio">
+            <span class="plan-details" aria-hidden="true">
+            <img src="/storage/promotion/Central_Giftcard.png" />
+            </span>
+        </label>
     </div>
-    <div class="controls-wrapper"></div>
+    <span class="span_error"></span>
 </div>
 
 
