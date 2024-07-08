85a3b8e8993a823c21ffbbaab7483ae65c4ce979
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2024-07-08 17:13:17 +0700

    pa promotion #7

--- resources/views/frontend/component/form-birthdate-pa.blade.php
+++ resources/views/frontend/component/form-birthdate-pa.blade.php
@@ -6,7 +6,24 @@
         
         @if($controller == 'product')
             <input type="hidden" id="promotion_code_condition" value="{{config('project.promotion_code_condition')}}"/>
-            @include('frontend.component.form-input-pa-promotion')
+            <strong>@lang('product.PromotionCode.promotion_code_title')</strong>
+            <div class="promotion-code">
+                <div class="grid">
+                    <label class="card">
+                        <input name="plan" class="radio" type="radio" checked>
+                        <span class="plan-details">
+                        <img src="/storage/promotion/Starbucks_Giftcard.png" />
+                        </span>
+                    </label>
+                    <label class="card">
+                        <input name="plan" class="radio" type="radio">
+                        <span class="plan-details" aria-hidden="true">
+                        <img src="/storage/promotion/Central_Giftcard.png" />
+                        </span>
+                    </label>
+                </div>
+                <span class="span_error"></span>
+            </div>
         @endif
 
         <div class="controls-wrapper full">
