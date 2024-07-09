717b7bbff95d83cfef52136078d431db1db2240a
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2024-07-09 09:36:13 +0700

    pa promotion #9

--- resources/views/frontend/component/form-input-pa-promotion.blade.php
+++ resources/views/frontend/component/form-input-pa-promotion.blade.php
@@ -1,21 +1,18 @@
 <strong>@lang('product.PromotionCode.promotion_code_title')</strong>
 <div class="promotion-code">
-    <div class="grid-promotion">
-        <label class="card-promotion">
-            <input name="plan" class="radio-promotion" type="radio" checked>
-            <span class="plan-details-promotion">
+    <div class="grid">
+        <div class="controls-wrapper full">
+            <input id="promotion" name="promotion" type="radio" checked/>
             <img src="/storage/promotion/Starbucks_Giftcard.png" />
-            </span>
-        </label>
-        <label class="card-promotion">
-            <input name="plan" class="radio-promotion" type="radio">
-            <span class="plan-details-promotion" aria-hidden="true">
-            <img src="/storage/promotion/Central_Giftcard.png" />
-            </span>
-        </label>
+            <span class="span_error"></span>
+        </div>
+        <div class="controls-wrapper full">
+            <input id="promotion" name="promotion" type="radio"/>
+            <img src="/storage/promotion/Starbucks_Giftcard.png" />
+            <span class="span_error"></span>
+        </div>
     </div>
     <span class="span_error"></span>
 </div>
-
 
 	
