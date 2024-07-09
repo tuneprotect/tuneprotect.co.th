b1b6c484a53a0be781f50062b11dd7c209a80bec
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2024-07-09 08:20:21 +0700

    pa promotion #8

--- resources/views/frontend/component/form-birthdate-pa.blade.php
+++ resources/views/frontend/component/form-birthdate-pa.blade.php
@@ -6,24 +6,7 @@
         
         @if($controller == 'product')
             <input type="hidden" id="promotion_code_condition" value="{{config('project.promotion_code_condition')}}"/>
-            <strong>@lang('product.PromotionCode.promotion_code_title')</strong>
-            <div class="promotion-code">
-                <div class="grid">
-                    <label class="card">
-                        <input name="plan" class="radio" type="radio" checked>
-                        <span class="plan-details">
-                        <img src="/storage/promotion/Starbucks_Giftcard.png" />
-                        </span>
-                    </label>
-                    <label class="card">
-                        <input name="plan" class="radio" type="radio">
-                        <span class="plan-details" aria-hidden="true">
-                        <img src="/storage/promotion/Central_Giftcard.png" />
-                        </span>
-                    </label>
-                </div>
-                <span class="span_error"></span>
-            </div>
+            @include('frontend.component.form-input-pa-promotion')
         @endif
 
         <div class="controls-wrapper full">
--- resources/views/frontend/component/form-input-pa-promotion.blade.php
+++ resources/views/frontend/component/form-input-pa-promotion.blade.php
@@ -1,15 +1,15 @@
 <strong>@lang('product.PromotionCode.promotion_code_title')</strong>
 <div class="promotion-code">
-    <div class="grid">
-        <label class="card">
-            <input name="plan" class="radio" type="radio" checked>
-            <span class="plan-details">
+    <div class="grid-promotion">
+        <label class="card-promotion">
+            <input name="plan" class="radio-promotion" type="radio" checked>
+            <span class="plan-details-promotion">
             <img src="/storage/promotion/Starbucks_Giftcard.png" />
             </span>
         </label>
-        <label class="card">
-            <input name="plan" class="radio" type="radio">
-            <span class="plan-details" aria-hidden="true">
+        <label class="card-promotion">
+            <input name="plan" class="radio-promotion" type="radio">
+            <span class="plan-details-promotion" aria-hidden="true">
             <img src="/storage/promotion/Central_Giftcard.png" />
             </span>
         </label>
