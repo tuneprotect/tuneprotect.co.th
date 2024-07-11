b9967e42e57c6ef8ca9581e5d2197a82a18ddac3
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2024-07-11 13:29:45 +0700

    pa promotion #30

--- resources/js/product/taipoct22.js
+++ resources/js/product/taipoct22.js
@@ -641,10 +641,11 @@ document.addEventListener("DOMContentLoaded", async () => {
                             
                         }
 
-                        let startDate = parseISO(data.fdFromDate);
-                        let endDate = parseISO(data.fdToDate);
+                        let current_date = (new Date().toLocaleDateString()).split('/');
+                        let effectiveDate = parseISO(data.fdFromDate);
+                        let currentDate = parseISO(`${current_date[2]}-${current_date[1]}-${current_date[0]}`);
 
-                        promotion_extra = differenceInDays(endDate, startDate);
+                        promotion_extra = differenceInDays(effectiveDate, currentDate);
                         console.log(promotion_extra);
                         
                         for (let i = 1; i <= $('#ctrl_no_of_insured').value; i++) {
