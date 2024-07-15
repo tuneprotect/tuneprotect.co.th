2e15e2c87ab7c3b6d373176ccc551f7aa3c16481
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2024-07-15 14:06:05 +0700

    pa promotion #38

--- resources/js/product/onpaca.js
+++ resources/js/product/onpaca.js
@@ -698,9 +698,9 @@ document.addEventListener("DOMContentLoaded", async () => {
                         ${data.fdRevenue === 'Y' ? '<div><span>' + $('label[for=fdTaxno]').innerText + ' : </span><strong>' + data.fdTaxno + '</strong></div>' : ''}
                         <div><span>${$('#receve_channel_title').innerText} : </span><strong>${data.fdSendType === 'P' ? $('label[for=ctrl_channel_post]').innerText : $('label[for=ctrl_channel_email]').innerText}</strong></div>
                     
-                        ${$('#controller').value === 'product' && promotionCodeStatus && i == 0
+                        ${$('#controller').value === 'product' && promotionCodeStatus
                             ? `<div class="controls-wrapper full no-lable"><span>${$('#lblfdPromotionCode').innerText} : </span><strong>${$("input[name=fdPromotionCode]:checked").value} ${ promotion_data.result.status ? `
-                            ${ promotion_data.result.codeAvailable < i+1 
+                            ${ promotion_data.result.codeAvailable < 1 
                                 ? `<span id="promotion_code_alert" style="color: #e71618;">${locale === 'th' ? '(* โค้ดนี้ได้ถูกใช้ครบแล้ว)' : '(* The code has already been used.)'}</span>` 
                                 : `<span id="promotion_code_alert" style="color: #008b06;">${locale === 'th' ? '('+ promotion_data.result.message_th +')' : '('+ promotion_data.result.message +')'}`}</span>` 
                                 : `<span id="promotion_code_alert" style="color: #e71618;">${locale === 'th' ? '(* '+ promotion_data.result.message_th +')' : '(* '+ promotion_data.result.message +')'}</span>` } </strong>
