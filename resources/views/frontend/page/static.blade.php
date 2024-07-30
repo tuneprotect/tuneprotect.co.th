75d8ea4797fe3a8884441434e5437275cde5c1b5
TUNEINSURANCE\aphichat.t <aphichat.t@tuneprotect.com>
2024-07-30 11:13:36 +0700

    update button share

--- resources/views/frontend/page/static.blade.php
+++ resources/views/frontend/page/static.blade.php
@@ -1,6 +1,7 @@
 @extends('frontend.layout.main')
 
 @section('page')
+<script src="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"></script>
     <main>
         <article class="wrapper">
             @if(isset($articleImage))
@@ -14,11 +15,31 @@
                 <div>{!! $content->locales[$locale]->content !!}</div>
             <br/>
             @if(!isset($no_share))
-                <script type='text/javascript'
+                <!-- <script type='text/javascript'
                         src='https://platform-api.sharethis.com/js/sharethis.js#property=5fe33658948afa0012592b2d&product=inline-share-buttons'
                         async='async'></script>
-                <div class="sharethis-inline-share-buttons"></div>
-                <br/>
+                <div class="sharethis-inline-share-buttons"></div> -->
+
+            <div class="social-share-btns-container">  
+                <div class="social-share-btns">
+                    <a class="share-btn.share-btn-twitter" href="https://twitter.com/intent/tweet?text=https://codepen.io/marko-zub/#" rel="nofollow" target="_blank">
+                        <i class="ion-social-twitter">Tweet</i>
+                    </a>
+                    <a class="share-btn.share-btn-facebook" href="https://www.facebook.com/sharer/sharer.php?u=https://codepen.io/marko-zub/#" rel="nofollow" target="_blank">
+                        <i class="ion-social-facebook">Share</i>
+                    </a>
+                    <a class="share-btn.share-btn-linkedin" href="https://www.linkedin.com/cws/share?url=https://codepen.io/marko-zub/#" rel="nofollow" target="_blank">
+                        <i class="ion-social-linkedin">Share</i>
+                    </a>
+                    <a class="share-btn.share-btn-reddit" href="http://www.reddit.com/submit?url=https://codepen.io/marko-zub/&title=Marko+Zub+codepen" rel="nofollow" target="_blank">
+                        <i class="ion-social-reddit">Share</i>
+                    </a>
+                    <a class="share-btn.share-btn-mail" href="mailto:?subject=Look Fun Codepen Account&amp;body=https://codepen.io/marko-zub/#" rel="nofollow" target="_blank" title="via email">
+                        <i class="ion-paper-airplane">Share</i>
+                    </a>
+                </div>
+            </div>
+            <br/>
             @endif
         </article>
 
@@ -38,7 +59,7 @@
             @includeIf($extraComponent)
         @endif
 
-
+        
     </main>
 
 @endsection
